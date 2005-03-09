83b38011714edfada2d56116a99cdbf42e6734fa
duhlman <duhlman@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-03-09 21:53:47 +0000

    backport load_enum to legacy ordata

--- includes/ORDataObject.class.php
+++ includes/ORDataObject.class.php
@@ -152,5 +152,60 @@ class ORDataObject {
 		return $field_string;
 	}
 	
+	/**
+	 * Helper function that loads enumerations from the data as an array, this is also efficient
+	 * because it uses psuedo-class variables so that it doesnt have to do database work for each instance
+	 *
+	 * @param string $field_name name of the enumeration in this objects table
+	 * @param boolean $blank optional value to include a empty element at position 0, default is true
+	 * @return array array of values as name to index pairs found in the db enumeration of this field  
+	 */
+	function _load_enum($field_name,$blank = true) 
+	{
+		$table = "enumeration";
+		if ($this->enumTable != false) {
+			$table = $this->enumTable;
+		}
+		if (	isset($GLOBALS['static']['enums'][$table][$field_name]) 
+			&& is_array($GLOBALS['static']['enums'][$table][$field_name]) 
+		) { 
+			$enum = $GLOBALS['static']['enums'][$table][$field_name];
+		}
+		else 
+		{
+			$cols = $this->_db->MetaColumns($table);
+			if ($cols === false) {
+				$cols = $this->_db->MetaColumns("enumerations");
+			}
+			$enum = array();
+			if ($cols && !$cols->EOF) 
+			{
+				//why is there a foreach here? at some point later there will be a scheme to autoload all enums 
+				//for an object rather than 1x1 manually as it is now
+				foreach($cols as $col) 
+				{
+					if ($col->name == $field_name && substr($col->type,0,4) == "enum") {
+						preg_match_all("|[\'](.*)[\']|U",$col->type,$enum_types);
+						//position 1 is where preg_match puts the matches sans the delimiters
+						$enum = $enum_types[1];
+						//for future use
+						//$enum[$col->name] = $enum_types[1];
+					}
+				}
+			}
+			array_unshift($enum," ");
+			   
+			$enum = array_flip($enum);
+			$GLOBALS['static']['enums'][$table][$field_name] = $enum;
+		}	
+		//keep indexing consistent whether or not a blank is present
+		if (!$blank) 
+		{
+			unset($enum[" "]);
+		}
+		return $enum;
+	}
+	
+	
 } // end of ORDataObject
 ?>
