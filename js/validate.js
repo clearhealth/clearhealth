65188e701b66f79cf1879a9d940e7cca6b719ada
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-03-21 20:33:32 +0000

    email validation rule

--- js/validate.js
+++ js/validate.js
@@ -79,6 +79,48 @@ function clni_rule_match(element) {
 }
 
 
+/**
+ * Require tha the passed in element be an email address
+ */
+function clni_rule_email(element) {
+
+	var str = element.value;
+
+	var at="@"
+	var dot="."
+	var lat=str.indexOf(at)
+	var lstr=str.length
+	var ldot=str.indexOf(dot)
+	if (str.indexOf(at)==-1) {
+		return false
+	}
+
+	if (str.indexOf(at)==-1 || str.indexOf(at)==0 || str.indexOf(at)==lstr){
+		return false
+	}
+
+	if (str.indexOf(dot)==-1 || str.indexOf(dot)==0 || str.indexOf(dot)==lstr){
+		return false
+	}
+
+	if (str.indexOf(at,(lat+1))!=-1){
+		return false
+	}
+
+	if (str.substring(lat-1,lat)==dot || str.substring(lat+1,lat+2)==dot){
+		return false
+	}
+
+	if (str.indexOf(dot,(lat+2))==-1){
+		return false
+	}
+		
+	if (str.indexOf(" ")!=-1){
+		return false
+	}
+
+	return true					
+}
 
 
 /**
