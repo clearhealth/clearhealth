196ac9971c43b4e2caae7799fdb8d43c484a3a16
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-03-21 18:45:08 +0000

    Fix for bug 167 let users change there own password

--- local/controllers/C_User.class.php
+++ local/controllers/C_User.class.php
@@ -88,5 +88,26 @@ class C_User extends Controller {
 	}
 
 
+	/**
+	 * Update hte password of the currently logged in user
+	 */
+	function password_action_edit() {
+		$user =& $this->_me->get_user();
+
+		$this->assign_by_ref('user',$user);
+		
+		return $this->fetch(Cellini::getTemplatePath("/user/" . $this->template_mod . "_password.html"));
+	}
+
+	function password_action_process() {
+		$user =& $this->_me->get_user();
+
+		if ($_POST['password']['current_password'] !== $user->get('password')) {
+			$this->messages->addMessage('Current Password Incorrect');
+			return "";
+		}
+		$user->set('password',$_POST['password']['password']);
+		$user->persist();
+	}
 }
 ?>
