edb53aaa474dbd55de26ab05c3207151ffa8858c
duhlman <duhlman@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-04-05 08:55:56 +0000

    make grid display more legible

---
+++ local/includes/Grid_Renderer_AccountHistory.class.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once CELLINI_ROOT."/includes/Grid.class.php";
+
+class Grid_Renderer_AccountHistory extends Grid_Renderer_HTML {
+
+	function renderBody() {
+
+		$this->_grid->prepare();
+
+		$ret = $this->_renderTagWithAttributes('tbody','tbody',0)."\n";
+
+		$row = 0;
+		$ds =& $this->_grid->_datasource;
+		for($ds->rewind(); $ds->valid(); $ds->next()) {
+			$data = $ds->get();
+			$type = strtolower($data['type']);
+			
+			if ($type == "claim") {
+				$this->_grid->attributes[$row]['tr']['class'] = "alt";				
+			}
+			else {
+				unset($this->_grid->attributes[$row]['tr']['class']);
+			}
+			$ret .= $this->_renderTagWithAttributes('tr',$row,'tr');
+
+			if ($this->indexCol && $type == "claim") {
+				$ret .= $this->_renderTagWithAttributes('td',$row,'indexCol').($row+1)."</td>";
+				$row++;
+			}
+			elseif ($this->indexCol) {
+				$this->_grid->attributes[$row]['td']['indexCol']['class'];
+				$ret .= $this->_renderTagWithAttributes('td',$row,'indexCol')."</td>";
+			}
+
+			if (is_array($data)) {
+				foreach($ds->getRenderMap() as $index => $key) {
+					$ret .= $this->_renderTagWithAttributes('td',$row,$key)."$data[$key]</td>";
+				}
+			}
+			$ret .= "</tr>\n";
+			
+		}
+		$ret .= "</tbody>\n";
+		return $ret;
+	}
+
+}
+
+?>
