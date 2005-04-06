77f5bfd6cf6348e86d6a89bf1b12f7cd1ccf44d4
duhlman <duhlman@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-04-06 03:32:52 +0000

    linking of payments to code was broken, it was showing icd instead of cpt

--- local/controllers/C_Eob.class.php
+++ local/controllers/C_Eob.class.php
@@ -43,15 +43,13 @@ class C_Eob extends Controller {
 		$i = 0;
 		foreach($codeList as $code) {
 			$billList[$i]['code'] = $code['code'];
-			$billList[$i]['code_id'] = $code['code_id'];
+			$billList[$i]['code_id'] = $code['parent_id'];
 			$billList[$i]['description'] = $code['description'];
 			$billList[$i]['amount'] = $code['fee'];
 			$billList[$i]['paid'] = 0;
 			$billList[$i]['writeoff'] = 0;
-			$billList[$i]['current_paid'] = $payment->totalPaidForCodeId($code['code_id']);
-			$billList[$i]['current_writeoff'] = $payment->totalWriteoffForCodeId($code['code_id']);
-
-
+			$billList[$i]['current_paid'] = $payment->totalPaidForCodeId($code['parent_id']);
+			$billList[$i]['current_writeoff'] = $payment->totalWriteoffForCodeId($code['parent_id']);
 			$billList[$i]['carry'] = $billList[$i]['amount'] - $billList[$i]['current_paid'] - $billList[$i]['current_writeoff'];
 
 			$i++;
@@ -82,6 +80,8 @@ class C_Eob extends Controller {
 
 		$total_paid = 0;
 		$total_writeoff = 0;
+
+		
 
 		if (isset($_POST['bill']) && count($_POST['bill']) > 0) {
 			foreach($_POST['bill'] as $line) {
