dd804d1b023a3b0417b1435593db88a3c3960b5f
duhlman <duhlman@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-05-02 07:00:33 +0000

    cleanup revno echo

--- nopkg/package_clearhealth.sh
+++ nopkg/package_clearhealth.sh
@@ -5,7 +5,7 @@ svn checkout --username ftrotter --password 4fred  https://svn2.uversainc.com/sv
 svn checkout --username ftrotter --password 4fred  https://svn2.uversainc.com/svn/cellini/trunk/ freeb2/cellini
 pwd
 revnum=`/usr/bin/svnversion clearhealth | cut -f '2' -d ':'`
-echo "<?php echo ' \"$revnum\" <br>'; ?>" > clearhealth/revision.php
+echo "<?php echo \"$revnum\"; ?>" > clearhealth/revision.php
 find -name ".svn" | xargs -i rm -rf {}
 find -name "nopkg*" | xargs -i rm -rf {}
 find ./clearhealth/local/setup/ -name "clearhealth*"  | xargs -i cat {} >> ./clearhealth/local/setup/install.sql
