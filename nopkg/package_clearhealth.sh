991ab7bd7046189dd1fb26ed19975d94d7f0e191
ftrotter <ftrotter@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-05-12 21:35:14 +0000

    added some helpers to the packaging script

--- nopkg/package_clearhealth.sh
+++ nopkg/package_clearhealth.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# To make this packaging script work you must
+# add you svn username and password
+# uncomment the .svn and nopkg remover
+# change nobody to your apache user 
+# run the whole thing as root...
 svn checkout --username username --password password  https://svn2.uversainc.com/svn/clearhealth/clearhealth/trunk/ clearhealth
 svn checkout --username username --password password  https://svn2.uversainc.com/svn/cellini/trunk/ clearhealth/cellini
 svn checkout --username username --password password  https://svn2.uversainc.com/svn/freeb2/trunk/ clearhealth/freeb2
@@ -10,4 +15,6 @@ echo "<?php echo \"$revnum\"; ?>" > clearhealth/revision.php
 #find -name "nopkg*" | xargs -i rm -rf {}
 /bin/rm -rf ./clearhealth/local/setup/install.sql
 find ./clearhealth/local/setup/ -maxdepth 1 -name "clearhealth*"  | tee sqlfilelist | xargs -i cat {} >> ./clearhealth/local/setup/install.sql
+chown -R nobody:nobody clearhealth/tmp
+chown -R nobody:nobody clearhealth/freeb2/tmp
 tar -czvf clearhealth.$revnum.tgz clearhealth/ 
