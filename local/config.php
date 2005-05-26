39d00f4970611e8be7fbef7f4dd755931efec140
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-05-26 22:51:03 +0000

    Add comments to config and attempt to rearrange it into a more useful layout, also add config options for group_appointments

--- local/config.php
+++ local/config.php
@@ -1,40 +1,77 @@
 <?php
-$config['debug'] = false;
+// Cellini Setup, installtion should only require editing of: 
+// App Setup, Database Setup, and possible freeb2 integration
+
+################################################################################
+# App Setup
+################################################################################
+// Session Name
+$config['app_name'] = "clearhealth";
+
+// default starting location /index.php/calendar/day
+$config['default_controller'] = "calendar";
+$config['default_action'] = "day";
+
+// where forms are uploaded too
+$config['user_forms_dir'] = APP_ROOT."/user/forms/";
+
+// where documents are uploaded too
+$config['document_manager']['repository'] = APP_ROOT."/user/documents/";
+
+// location of htmldoc utility for creating pds
+$config['htmldoc'] = "/usr/bin/htmldoc";
+
+// Do you want an option for group appointments on the calendar
+$config['scheduling']['group_appointments'] = true;
+
+################################################################################
+# Database Setup
+################################################################################
 $config['db_type'] = "mysql";
 $config['db_host'] = "localhost";
 $config['db_user'] = "root";
 $config['db_password'] = "";
 $config['db_name'] = "clearhealth";
 $config['db_table_prefix'] = "";
+
+
+################################################################################
+# freeb2 integration
+################################################################################
+$GLOBALS['C_ALL']['freeb2_dir'] = "/clearhealth/freeb2/index.php/";
+$config['translate']['freeb2'] = "/clearhealth/freeb2/index.php/";
+
+
+################################################################################
+# Cellini Options 
+################################################################################
+// global cellini options
 $config['require_login'] = true;
+$config['use_text_storage'] = true;
+$config['autoAcl'] = true;
 
-$config['default_controller'] = "calendar";
-$config['default_action'] = "day";
+// menu config
 $config['use_menu'] = true;
+$config['menu']['attachReports'] = true;
+$config['menu']['attachForms'] = true;
 
-$config['document_manager']['repository'] = APP_ROOT."/user/documents/";
+// document management
 $config['document_manager']['documents_on_tree'] = true;
 $config['document_manager']['category_view'] = false;
 $config['document_manager']['type_type'] = false;
 
+// Show extra acl and db debugging info
+$config['debug'] = false;
+
+################################################################################
+# Other Options
+################################################################################
+
+// Openemr compat should be unused
 $config['template_dir'] = APP_ROOT."/local/templates/";
 $GLOBALS['template_dir'] = APP_ROOT."/local/templates/";
 $config['openemr_db'] = 'openemr';
-$config['autoAcl'] = true;
 
-$config['user_forms_dir'] = APP_ROOT."/user/forms/";
-
-$config['translate']['freeb2'] = "/clearhealth/freeb2/index.php/";
-
-$config['menu']['attachReports'] = true;
-$config['menu']['attachForms'] = true;
-
-$config['app_name'] = "clearhealth";
-
-$config['freeb2_wsdl'] = "http://localhost/clearhealth/freeb2/soap_gateway.php?wsdl";
-$GLOBALS['C_ALL']['freeb2_dir'] = "/clearhealth/freeb2/index.php/";
+// ???
 $GLOBALS['C_ALL']['emr_dir'] = "/index.php/";
-
-$config['htmldoc'] = "/usr/bin/htmldoc";
-$config['use_text_storage'] = true;
 ?>
