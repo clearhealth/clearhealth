f3c2b51fe8d82b973a8e96c8f932f0de4c8321f8
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-06-15 20:03:28 +0000

    Census report

---
+++ local/setup/reports/Census.sql
@@ -0,0 +1,66 @@
+---[census]---
+select
+ concat(p.last_name,', ',p.first_name) Patient,
+ o.reason_code Reason,
+ if (o.walkin,'Y','N') `Walk-in?`,
+ date_format(o.start,'%a %m/%d/%Y') `Date`,
+ concat_ws(' to ',date_format(o.start,'%H:%i'),date_format(o.end,'%H:%i')) `Time`
+from
+ occurences o
+ inner join person p on o.external_id = p.person_id
+ inner join encounter e on e.occurence_id = o.id
+where
+ if ('[after]',o.start > '[after:date]',1) and
+ if ('[before]',o.end < '[before:date]',1) and
+ if ('[facility]',o.location_id = '[facility:query:select r.id, concat(r.name,'->',b.name) name from rooms r inner join buildings b on r.building_id = b.id]',1) and
+ if ('[provider]',o.user_id = '[provider:query:select u.user_id, concat(p.last_name,', ',p.first_name) name from user u inner join person p on u.person_id = p.person_id]',1)
+order by
+ `Date` DESC, `Time` DESC
+/***
+dsFilters-Reason|enumLookup&ds|appointment_reasons
+***/
+---[total_encounters,hideFilter,noPager]---
+select
+ count(e.encounter_id) `Total Encounters`
+from
+ occurences o
+ inner join person p on o.external_id = p.person_id
+ inner join encounter e on e.occurence_id = o.id
+where
+ if ('[after]',o.start > '[after:date]',1) and
+ if ('[before]',o.end < '[before:date]',1) and
+ if ('[facility]',o.location_id = '[facility:query:select r.id, concat(r.name,'->',b.name) name from rooms r inner join buildings b on r.building_id = b.id]',1) and
+ if ('[provider]',o.user_id = '[provider:query:select u.user_id, concat(p.last_name,', ',p.first_name) name from user u inner join person p on u.person_id = p.person_id]',1)
+---[total_encounters_by_reason,hideFilter,noPager]---
+select
+ o.reason_code `Reason`,
+ count(e.encounter_id) `Total`
+from
+ occurences o
+ inner join person p on o.external_id = p.person_id
+ inner join encounter e on e.occurence_id = o.id
+where
+ if ('[after]',o.start > '[after:date]',1) and
+ if ('[before]',o.end < '[before:date]',1) and
+ if ('[facility]',o.location_id = '[facility:query:select r.id, concat(r.name,'->',b.name) name from rooms r inner join buildings b on r.building_id = b.id]',1) and
+ if ('[provider]',o.user_id = '[provider:query:select u.user_id, concat(p.last_name,', ',p.first_name) name from user u inner join person p on u.person_id = p.person_id]',1)
+group by
+ o.reason_code
+/***
+dsFilters-Reason|enumLookup&ds|appointment_reasons
+***/
+---[total_encounters_by_walkin,hideFilter,noPager]---
+select
+ if (o.walkin,'Y','N') `Walk-in?`,
+ count(e.encounter_id) `Total`
+from
+ occurences o
+ inner join person p on o.external_id = p.person_id
+ inner join encounter e on e.occurence_id = o.id
+where
+ if ('[after]',o.start > '[after:date]',1) and
+ if ('[before]',o.end < '[before:date]',1) and
+ if ('[facility]',o.location_id = '[facility:query:select r.id, concat(r.name,'->',b.name) name from rooms r inner join buildings b on r.building_id = b.id]',1) and
+ if ('[provider]',o.user_id = '[provider:query:select u.user_id, concat(p.last_name,', ',p.first_name) name from user u inner join person p on u.person_id = p.person_id]',1)
+group by
+ o.walkin
