98bdc8482cb03056caf2d90b1032ef8a5a62f875
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-06-24 17:42:10 +0000

    Add exit report to svn so we don't lose it

---
+++ local/setup/reports/ExitReport.sql
@@ -0,0 +1,82 @@
+---[practice]---
+select 
+ p.name,
+ a.line1,
+ a.line2,
+ a.city,
+ a.state,
+ a.postal_code
+from practices p 
+inner join buildings b on p.id = b.practice_id
+inner join encounter e on b.id = e.building_id
+left join practice_address pa on p.id = pa.practice_id
+left join address a using(address_id)
+where address_type = 4 and e.encounter_id = '[encounter_id:GET]'
+---[treating_facility]---
+select 
+ b.name,
+ a.line1,
+ a.line2,
+ a.city,
+ a.state,
+ a.postal_code
+from buildings b
+inner join encounter e on b.id = e.building_id
+left join building_address ba on b.id = ba.building_id
+left join address a using(address_id)
+where e.encounter_id = '[encounter_id:GET]'
+---[treating_provider]---
+select 
+ per.salutation,
+ per.last_name,
+ per.first_name,
+ p.state_license_number,
+ a.line1,
+ a.line2,
+ a.city,
+ a.state,
+ a.postal_code,
+ n.number
+
+from provider p
+inner join person per using(person_id)
+inner join encounter e on p.person_id = e.treating_person_id
+left join person_address pa on p.person_id = pa.person_id
+left join address a using(address_id)
+left join person_number pn on p.person_id = pn.person_id
+left join number n using(number_id)
+where n.number_type = 1 and address_type =1  and e.encounter_id = '[encounter_id:GET]'
+---[patient]---
+select * from person p
+inner join patient pat using(person_id)
+inner join encounter e on p.person_id = e.patient_id
+left join person_address pa on p.person_id = pa.person_id
+left join address a using(address_id)
+left join person_number pn on p.person_id = pn.person_id
+left join number n using(number_id)
+where n.number_type = 1 and address_type =1  and e.encounter_id = '[encounter_id:GET]'
+---[code_list]--- 
+select cpt.code_text `Procedure`, cpt.code Code, 
+concat_ws(', '
+,max(case code_order when 1 then c.code else null end) 
+,max(case code_order when 2 then c.code else null end)
+,max(case code_order when 3 then c.code else null end)
+,max(case code_order when 4 then c.code else null end) 
+) Diagnosis, cd.modifier, cd.units, cd.fee
+from coding_data cd
+inner join codes c using(code_id)
+inner join codes cpt on cd.parent_id = cpt.code_id
+inner join encounter e on cd.foreign_id = e.encounter_id
+where e.encounter_id = '[encounter_id:GET]'
+group by cd.parent_id
+union
+select 'Total','','',null,sum(units),sum(fee)
+from coding_data cd
+where foreign_id = '[encounter_id:GET]' and primary_code = 1
+---[payment_history]---
+select 
+payment_date, amount, payment_type
+from payment
+where encounter_id = '[encounter_id:GET]'
+---[encounter]---
+select * from encounter e where e.encounter_id = '[encounter_id:GET]'
