87f4dece3de3ce24ee3506153388d7830aaa6933
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-07-07 21:20:38 +0000

    script to fix db for bug 1107

---
+++ local/scripts/fix_record_numbers.php
@@ -0,0 +1,29 @@
+<?php
+// fixes record # for patients who have a record id of 0
+// all are queried and replaced with the next record # sequence
+
+include "../../cellini/bootstrap.php";
+
+
+$db = Cellini::dbInstance();
+
+$res = $db->execute("select person_id from patient where record_number = 0");
+
+$count = 0;
+while($res && !$res->EOF) {
+	unset($p);
+
+	$p =& ORDataObject::Factory('Patient',$res->fields['person_id']);
+
+	$p->set('record_number',$p->generate_record_number());
+	$p->persist();
+
+	$count++;
+	echo "Updated: ".$p->get('person_id')."\n";
+	flush();
+
+	$res->MoveNext();
+}
+
+echo "\n\n$count Patient's Updated\n";
+?>
