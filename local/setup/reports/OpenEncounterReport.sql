8ebec16b30db15c7cfafed15aaa3645179b65aae
tswicegood <tswicegood@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-07-19 23:36:11 +0000

    Updated OpenEncounterReport to address issue 1070
    Adds Facility (buildings.name) to default report template and SQL.

--- local/setup/reports/OpenEncounterReport.sql
+++ local/setup/reports/OpenEncounterReport.sql
@@ -1,8 +1,12 @@
-select 
+SELECT 
 	date_format(date_of_treatment,'%Y-%m-%d') date_of_treatment, 
 	date_format(timestamp,'%Y-%m-%d') last_change, 
 	concat_ws(', ',p.last_name,p.first_name) patient,
-	encounter_id 
-from encounter 
-inner join person p on patient_id = person_id 
-where status = 'open'
+	e.encounter_id,
+	b.name AS "Facility"
+FROM
+	encounter AS e
+	INNER JOIN person AS p on e.patient_id = p.person_id
+	JOIN buildings AS b ON(b.id = e.building_id)
+WHERE
+	e.status = 'open'
