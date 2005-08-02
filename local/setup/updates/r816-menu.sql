24604c4c1f3e7ac15cea1172ecc80ddbd3d2e266
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-08-02 23:06:01 +0000

    menu entry for the summary report

---
+++ local/setup/updates/r816-menu.sql
@@ -0,0 +1,5 @@
+INSERT INTO `menu` ( `menu_id` , `site_section` , `parent` , `dynamic_key` , `section` , `display_order` , `title` , `action` , `prefix` )
+VALUES (
+'', 'patient', '44', '', 'children', '5', 'Summary Report', 'Patient/summary_report', 'main'
+);
+
