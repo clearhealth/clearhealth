4b9c04768010fc7bbeefd35ae3870c1393a24e58
duhlman <duhlman@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-08-23 23:04:32 +0000

    rename to make more sense

--- local/setup/import/import_openemr_data_nightly.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-php squeeze_openemr_patientdata.php > patient_dataset.php
-php inject_patientdata.php
-rm -rf patient_dataset.php
-php squeeze_op-en-hcs_calendar.php > calendar_dataset.php
-php inject_calendar.php
-rm -rf calendar_dataset.php
