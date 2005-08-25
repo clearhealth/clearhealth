6456ef336f67003b9fb54db8f10967699e602f4f
cpowers <cpowers@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-08-25 23:19:01 +0000

    Added ASP style hosting instance config Added installer config files Added config.php.dist installer template Removed config.php

---
+++ local/setup/config_instances.php
@@ -0,0 +1,96 @@
+<?php
+// This config file will load the database settings based
+// on the URL of the site. It should be used when you want
+// to run multiple instances of clearhealth using the same
+// code base.
+//
+// Cellini Setup, installtion should only require editing of: 
+// App Setup, Database Setup, and possible freeb2 integration
+
+// Get the instance name from the URL path
+$path_parts = split('/',$_SERVER['REQUEST_URI']);
+$instance_name = $path_parts[1];
+$instance_config_dir = dirname(__FILE__).'/setup/instances';
+$instance_config_file = $instance_config_dir.'/'.$instance_name.'.php';
+
+################################################################################
+# App Setup
+################################################################################
+// Session Name
+$config['app_name'] = "clearhealth";
+
+// default starting location /index.php/calendar/day
+$config['default_controller'] = "calendar";
+$config['default_action'] = "day";
+
+// where forms are uploaded too
+$config['user_forms_dir'] = APP_ROOT."/user/forms/";
+
+// where documents are uploaded too
+$config['document_manager']['repository'] = APP_ROOT."/user/documents/";
+
+// location of htmldoc utility for creating pds
+$config['htmldoc'] = "/usr/bin/htmldoc";
+
+// Do you want an option for group appointments on the calendar
+$config['scheduling']['group_appointments'] = false;
+
+################################################################################
+# Database Setup
+################################################################################
+$config['db_type'] = "mysql";
+$config['db_host'] = "localhost";
+$config['db_user'] = "root";
+$config['db_password'] = "";
+$config['db_name'] = "clearhealth";
+$config['db_table_prefix'] = "";
+
+
+################################################################################
+# freeb2 integration
+################################################################################
+$GLOBALS['C_ALL']['freeb2_dir'] = "/clearhealth/freeb2/index.php/";
+$config['translate']['freeb2'] = "/clearhealth/freeb2/index.php/";
+
+
+################################################################################
+# Cellini Options 
+################################################################################
+// global cellini options
+$config['require_login'] = true;
+$config['use_text_storage'] = true;
+$config['autoAcl'] = true;
+
+// menu config
+$config['use_menu'] = true;
+$config['menu']['attachReports'] = true;
+$config['menu']['attachForms'] = true;
+
+// document management
+$config['document_manager']['documents_on_tree'] = true;
+$config['document_manager']['category_view'] = false;
+$config['document_manager']['type_type'] = false;
+
+// Show extra acl and db debugging info
+$config['debug'] = false;
+
+################################################################################
+# Other Options
+################################################################################
+
+// Openemr compat should be unused
+$config['template_dir'] = APP_ROOT."/local/templates/";
+$GLOBALS['template_dir'] = APP_ROOT."/local/templates/";
+$config['openemr_db'] = 'openemr';
+
+// ???
+$GLOBALS['C_ALL']['emr_dir'] = "/index.php/";
+
+// template mappings
+$GLOBALS['C_ALL']['group_appointments'] = $config['scheduling']['group_appointments'];
+
+// Load instance specific configuration information.
+if(!is_readable($instance_config_file)){
+	die("Error loading instance configuration file $instance_config_file");
+}
+?>
---
+++ local/setup/installer/versions.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Created on Aug 16, 2005
+ *
+ * Example version file for application installer
+ */
+$base_app_path = realpath(dirname(__FILE__).'/../');
+
+$versions = new VersionSet();
+
+$version_1rc2 = new Version('1.0RC2');
+$version_1rc2->collectData('db_user', 'Database Username', 'text', 'root');
+$version_1rc2->collectData('db_password', 'Database Password', 'text', '');
+$version_1rc2->collectData('db_server', 'Database Server', 'text', 'localhost');
+$version_1rc2->collectData('db_database', 'Database Name', 'text', '');
+$version_1rc2->addTest('PHPVersion', array('4.3.0'));
+$version_1rc2->addTest('PHPExtension', array('mysql'));
+$version_1rc2->addTest('WritableLocation', array($base_app_path.'/tmp'));
+$version_1rc2->addTest('MysqlVersion', array(
+'username_field' => 'db_user',
+'password_field' => 'db_password',
+'server_field' => 'db_server',
+'port_field' => 'db_port',
+'version' => '4.1.0'));
+$version_1rc2->addAction('AcceptText', array(dirname(__FILE__).'/LICENSE'));
+$version_1rc2->addAction('SQLFile', array(
+'username_field' => 'db_user',
+'password_field' => 'db_password',
+'server_field' => 'db_server',
+'port_field' => 'db_port',
+'db_field' => 'db_database',
+'files' => array($base_app_path.'/local/setup/clearhealth-0.1RC2.sql')));
+$version_1rc2->addAction('ReplaceString', array(
+'message' => "Saved database configuration information!",
+'files' => array($base_app_path.'/local/config.php.dist' => $base_app_path.'/local/config.php'),
+'fields' => array('INSTALL_DB_USERNAME' => 'db_user', 'INSTALL_DB_PASSWORD' => 'db_password', 'INSTALL_DB_DATABASE' => 'db_database', 'INSTALL_DB_SERVER' => 'db_server')
+));
+
+$versions->add($version_1rc2);
+?>
