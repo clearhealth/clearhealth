22902c8bd5499ea04b5b4e0cb771b1dcf0e9f61f
ftrotter <ftrotter@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-09-20 00:08:02 +0000

    added enumeration support for hcfa comment fields

--- local/setup/updates/980.sql
+++ local/setup/updates/980.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `enumeration` ADD `encounter_value_type` ENUM( 'medicaid_resubmission_code', 'prior_authorization_number', 'auto_accident_state', 'original_reference_number' ) NOT NULL
+ALTER TABLE `enumeration` ADD `encounter_value_type` ENUM( 'hcfa_19_comment', 'hcfa_10d_comment', 'medicaid_resubmission_code', 'prior_authorization_number', 'auto_accident_state', 'original_reference_number' ) NOT NULL
