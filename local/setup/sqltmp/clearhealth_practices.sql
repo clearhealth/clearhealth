687e25637c6058d14767f0523bbc6d8432cb3b80
tswicegood <tswicegood@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-10-25 14:39:09 +0000

    Corrects typo in practices table definition and adds alter code for previously installed systems.
    Fixes issue 4334

--- local/setup/sqltmp/clearhealth_practices.sql
+++ local/setup/sqltmp/clearhealth_practices.sql
@@ -2,6 +2,6 @@ CREATE TABLE `practices` (
   `id` int(11) NOT NULL default '0',
   `name` varchar(255) NOT NULL default '',
   `website` varchar(255) NOT NULL default '',
-  `identifer` varchar(255) NOT NULL default '',
+  `identifier` varchar(255) NOT NULL default '',
   PRIMARY KEY  (`id`)
 ) TYPE=MyISAM;
