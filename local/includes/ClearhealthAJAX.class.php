cf975825768d9f36e83a7abb8300b57c4f64e3be
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-11-07 16:39:04 +0000

    update ajax export

--- local/includes/ClearhealthAJAX.class.php
+++ local/includes/ClearhealthAJAX.class.php
@@ -1,4 +1,11 @@
 <?php
 class ClearhealthAJAX {
+
+	function initActiveFeeSchedule() {
+		$GLOBALS['loader']->requireOnce('datasources/FeeSchedule_DS.class.php');
+		$ds =& new FeeSchedule_DS();
+
+		$this->server->registerClass($ds,'ActiveFeeSchedule',$ds->ajaxMethods());
+	}
 }
 ?>
