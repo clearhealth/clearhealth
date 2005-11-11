7b2da0189d8f8c7b293ce89819b7da45c781dd09
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-11-11 18:34:01 +0000

    Update a controller to the new require and action syntax, im going to try to do at least one of these a day from now on

--- local/controllers/C_Account.class.php
+++ local/controllers/C_Account.class.php
@@ -1,11 +1,16 @@
 <?php
 
-require_once CELINI_ROOT."/controllers/Controller.class.php";
-require_once CELINI_ROOT."/includes/Grid.class.php";
-require_once CELINI_ROOT."/includes/Datasource_sql.class.php";
-require_once APP_ROOT."/local/includes/Datasource_AccountHistory.class.php";
-require_once APP_ROOT."/local/includes/Grid_Renderer_AccountHistory.class.php";
+$loader->requireOnce("/controllers/Controller.class.php");
+$loader->requireOnce("/includes/Grid.class.php");
+$loader->requireOnce("/includes/Datasource_sql.class.php");
+$loader->requireOnce("/local/includes/Datasource_AccountHistory.class.php");
+$loader->requireOnce("/local/includes/Grid_Renderer_AccountHistory.class.php");
 
+/**
+ * Actions for working with an Account in Clearhealth, in this context this is a Patients Account
+ *
+ * Current contains an procedure/payment report
+ */
 class C_Account extends Controller {
 
 	var $filters = "";
@@ -21,10 +26,7 @@ class C_Account extends Controller {
 		$this->assign("filters",$this->filters);
 	}
 
-	function default_action() {
-	}
-	
-	function history_action_view($patient_id) {
+	function actionHistory_view($patient_id) {
 		
 		$hds =& new Datasource_AccountHistory($this->filters);
 		$hds->setup($patient_id);
@@ -41,7 +43,7 @@ class C_Account extends Controller {
 		return $this->fetch(Celini::getTemplatePath("/account/" . $this->template_mod . "_history.html"));
 	}
 
-	function history_action_process($patient_id) {
+	function processHistory_view($patient_id) {
 		$this->filters = $_SESSION['clearhealth']['filters'][get_class($this)] = $_POST['filter'];
 		$this->assign("filters",$this->filters);
 	}
