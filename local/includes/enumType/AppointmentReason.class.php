bf0dcd7a0bea5965bbc5e4d1d350b19622f29c2b
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-11-22 16:06:27 +0000

    appointment reason type

---
+++ local/includes/enumType/AppointmentReason.class.php
@@ -0,0 +1,22 @@
+<?php
+$loader->requireOnce('includes/enumType/PerPractice.class.php');
+/**
+ * Class the defines the default enumeration type
+ */
+class EnumType_AppointmentReason extends EnumType_PerPractice {
+
+	/**
+	 * Field info map, array of field names and types to use when editing
+	 */
+	var $definition = array(
+				'enumeration_value_id' => array('type'=>'hidden'),
+				'key' 	=> array('label'=>'Key','size'=>5), 
+				'value' => array('label'=>'Value','size'=>15),
+				'extra1' => array('label'=>'Template','type'=>'AppointmentTemplate'),
+				'extra2' => false,
+				'sort' => array('label'=>'Order','type'=>'order'),
+				'status' => array('label'=>'Enabled?','type'=>'boolean')
+			);
+
+}
+?>
