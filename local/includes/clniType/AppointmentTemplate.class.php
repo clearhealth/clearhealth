971ca9bd8a53e7225399913f0bf1337e1ff34a8d
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-11-22 16:07:03 +0000

    custom type which has a js widget for a template selector

---
+++ local/includes/clniType/AppointmentTemplate.class.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Defines a type that can be used in preferences or config, or any other place where a type needs to be user editable
+ *
+ * This api is still a work in progress
+ *
+ * @package	com.uversainc.celini
+ * @author	Joshua Eichorn <jeichorn@mail.com>
+ */
+class clniTypeAppointmentTemplate {
+	function label($id,$label) {
+		//return "<label for='$id'>$label</label>";
+		return $label;
+	}
+
+	function jsWidget($name) {
+
+		$db =& new clniDb();
+		$res = $db->execute("select * from appointment_template order by name");
+
+		$ret = "";
+		$ret .= "<option value=\"\">No Template</option>\n";
+		while($res && !$res->EOF) {
+			$ret .= '<option value="'.$res->fields['appointment_template_id'].'">'.$res->fields['name']."</option>\n";
+			$res->moveNext();
+		}
+		$ret = '<select name="'.$name.'">' . $ret . '</select>';
+		return $ret;
+	}
+
+	/**
+	 * Parses out the value from the widget returning it in the needed type
+	 */
+	function parseValue($input) {
+		return (int)$input;
+	}
+}
+?>
