eae9264de9387c786e82f3efafaca1e588185435
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-11-28 22:44:46 +0000

    menu additions for appointment templates

---
+++ local/setup/updates/2005-11-28_menu.sql
@@ -0,0 +1,5 @@
+INSERT INTO `menu` ( `menu_id` , `site_section` , `parent` , `dynamic_key` , `section` , `display_order` , `title` , `action` , `prefix` )
+VALUES ( '', 'admin', '5', '', 'children', '300', 'List Appointment Templates', 'AppointmentTemplate/list', 'main'); 
+INSERT INTO `menu` VALUES ( '', 'admin', '5', '', 'children', '310', 'Add Appointment Template', 'AppointmentTemplate/add', 'main');
+INSERT INTO `menu` VALUES ( '', 'admin', '5', '', 'children', '0', 'Edit Appointment Template', 'AppointmentTemplate/edit', 'main');
+
