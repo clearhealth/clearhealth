d8506478be5c2445a83360c76f33e9e7c1535118
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-11-29 16:56:24 +0000

    new tables, i thought i already had this in updates

--- local/setup/updates/2005-11-28_occurence_breakdown.sql
+++ local/setup/updates/2005-11-28_occurence_breakdown.sql
@@ -1,1 +1,11 @@
-ALTER TABLE `occurence_breakdown` ADD `index` INT AFTER `occurence_id` ;
+CREATE TABLE `occurence_breakdown` (
+  `occurence_breakdown_id` int(11) NOT NULL default '0',
+  `occurence_id` int(11) NOT NULL default '0',
+  `index` int(11) default '0',
+  `offset` int(11) NOT NULL default '0',
+  `length` int(11) NOT NULL default '0',
+  `user_id` int(11) NOT NULL default '0',
+  `title` varchar(255) NOT NULL default '',
+  PRIMARY KEY  (`occurence_breakdown_id`)
+) TYPE=MyISAM;
+
