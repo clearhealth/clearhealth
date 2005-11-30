d949a0482a362b85c45cd61981d3a50d8ba17bca
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-11-30 17:29:56 +0000

    Fix for 4637, note process functions weren't in the correct manager

--- local/controllers/M_Encounter.class.php
+++ local/controllers/M_Encounter.class.php
@@ -29,33 +29,5 @@ class M_Encounter extends Manager {
 		$this->controller->payment_id = $payment_id;
 	}
 
-	/**
-	 * Process a note
-	 */
-	function process_note($patient_id) {
-		$note_id = $_POST['note']['note_id'];
-		$note =& ORDataObject::factory('PatientNote',$note_id);
-		$note->populate_array($_POST['note']);
 
-		if ($note_id == 0) {
-			$note->set('user_id',$this->controller->_me->get_user_id());
-			$note->set('note_date',date('Y-m-d H:i:s'));
-		}
-
-		$note->set('patient_id',$patient_id);
-		$note->persist();
-		$this->controller->note_id = $note->get('id');
-	}
-	function process_depnote($patient_id,$note_id,$current) {
-		$note =& ORDataObject::factory('PatientNote',$note_id);
-
-		if ($current == 'Yes') {
-			$note->set('deprecated',0);
-		}
-		else {
-			$note->set('deprecated',1);
-		}
-
-		$note->persist();
-	}
 }
