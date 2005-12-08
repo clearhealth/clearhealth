739dbe8d50c21388b23eb12e3c66a11fb65cb55e
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-12-08 23:11:15 +0000

    as soon as i add the actual file in, i realize im missing an important field

--- local/setup/updates/2005-12-07_statement_history.sql
+++ local/setup/updates/2005-12-07_statement_history.sql
@@ -1,9 +1,10 @@
 CREATE TABLE `statement_history` (
   `statement_history_id` int(11) NOT NULL default '0',
+  `patient_id` INT NOT NULL,
   `statement_number` int(11) NOT NULL default '0',
   `date_generated` datetime NOT NULL default '0000-00-00 00:00:00',
   `amount` float(7,2) NOT NULL default '0.00',
   `type` int(11) NOT NULL default '0',
   PRIMARY KEY  (`statement_history_id`)
 )
-
+/* ALTER TABLE `statement_history` ADD `patient_id` INT NOT NULL AFTER `statement_history_id` ; */
