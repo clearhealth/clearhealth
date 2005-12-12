27e2d9637f71832d9872da57cfe6b78fb013cfea
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-12-12 23:20:53 +0000

    report snapshot ordo

---
+++ local/ordo/ReportSnapshot.class.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Object Relational Persistence Mapping Class for table: report_snapshot
+ *
+ * @package	com.uversainc.Celini
+ * @author	Joshua Eichorn <jeichorn@mail.com>
+ */
+
+$loader->requireOnce('ordo/ORDataObject.class.php');
+
+/**
+ * Object Relational Persistence Mapping Class for table: report_snapshot
+ *
+ * @package	com.uversainc.Celini
+ */
+class ReportSnapshot extends ORDataObject {
+
+	/**#@+
+	 * Fields of table: report_snapshot mapped to class members
+	 */
+	var $id			= '';
+	var $report_id		= '';
+	var $template_id	= '';	
+	var $snapshot_date	= false;
+	var $data		= '';
+	/**#@-*/
+
+
+	/**
+	 * Setup some basic attributes
+	 * Shouldn't be called directly by the user, user the factory method on ORDataObject
+	 */
+	function ReportSnapshot($db = null) {
+		parent::ORDataObject($db);	
+		$this->_table = 'report_snapshot';
+		$this->_sequence_name = 'sequences';	
+	}
+
+	/**
+	 * Populate the class from the db
+	 */
+	function populate() {
+		parent::populate('report_snapshot_id');
+	}
+
+	/**#@+
+	 * Getters and Setters for Table: report_snapshot
+	 */
+
+	
+	/**
+	 * Getter for Primary Key: report_snapshot_id
+	 */
+	function get_report_snapshot_id() {
+		return $this->id;
+	}
+
+	/**
+	 * Setter for Primary Key: report_snapshot_id
+	 */
+	function set_report_snapshot_id($id)  {
+		$this->id = $id;
+	}
+
+	function set_snapshot_date($date) {
+		$this->_setDate('snapshot_date', $date);
+	}
+	function get_snapshot_date() {
+		if ($this->snapshot_date === false) {
+			$this->_setDate('snapshot_date', date('Y-m-d H:i:s'));
+		}
+		return $this->_getTimestamp('snapshot_date');
+	}
+
+	/**#@-*/
+}
+?>
---
+++ local/setup/updates/2005-12-12_report_snapshot.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `report_snapshot` (
+  `report_snapshot_id` int(11) NOT NULL default '0',
+  `report_id` int(11) NOT NULL default '0',
+  `template_id` int(11) NOT NULL default '0',
+  `snapshot_date` datetime NOT NULL default '0000-00-00 00:00:00',
+  `data` longtext NOT NULL,
+  PRIMARY KEY  (`report_snapshot_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+        
+
