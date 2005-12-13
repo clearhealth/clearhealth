5dd133737f05f9880c40056a8a3d81754efa97d1
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-12-13 00:07:57 +0000

    update to include link to snapshotted patient statement

--- local/setup/reports/PatientStatementHistory.sql
+++ local/setup/reports/PatientStatementHistory.sql
@@ -10,9 +10,11 @@ where
 select
  statement_number `Statement #`,
  date_format(date_generated,'%m/%d/%Y') Date,
- amount
+ amount,
+concat('<a href="report?report_id=',report_id,'&template_id=',template_id,'&fromSnapshot=',sh.report_snapshot_id,'">View</a>') View 
 from
-statement_history
+statement_history sh
+ left join report_snapshot using(report_snapshot_id)
 where
  type = 1 and
  patient_id = '<<[patient_id:C_patient]>>'
