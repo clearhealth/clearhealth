8b96216d15938baa6f4bf6870ac9b035eef9284f
root <root@61f7cdb9-23f1-0310-8583-e3fc15525073>
2005-12-22 02:04:01 +0000

    Committed by Travis
    In case this information isn't available, just ignore this code.  Makes this work in code that doesn't set the defaultpractice.

--- local/includes/MenuArray.php
+++ local/includes/MenuArray.php
@@ -1,18 +1,20 @@
 <?php
 global $menuarrays;
 $menuarrays=array();
-$defPractice=&Celini::newOrdo('Practice',$_SESSION['defaultpractice']);
-$menuarrays[]=array(
-	'menus'=>array(112,113,114,115),
-	'sql'=>"
-				SELECT 
-					p.name as title,
-					CONCAT('".Celini::link(true,true,true)."','changepractice=',p.id) as action,
-					p.id as item_id
-				FROM
-					practices AS p
-				ORDER BY p.name",
-	'defaultitemid'=>$_SESSION['defaultpractice'],
-	'menutitle'=>'Practice: '.$defPractice->get('name')
-);
+if (isset($_SESSION['defaultpractice'])) {
+	$defPractice=&Celini::newOrdo('Practice',$_SESSION['defaultpractice']);
+	$menuarrays[]=array(
+		'menus'=>array(112,113,114,115),
+		'sql'=>"
+					SELECT 
+						p.name as title,
+						CONCAT('".Celini::link(true,true,true)."','changepractice=',p.id) as action,
+						p.id as item_id
+					FROM
+						practices AS p
+					ORDER BY p.name",
+		'defaultitemid'=>$_SESSION['defaultpractice'],
+		'menutitle'=>'Practice: '.$defPractice->get('name')
+	);
+}
 ?>
