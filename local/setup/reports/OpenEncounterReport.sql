ec18e44cbf70e4559e8c72dab94bcbb86bb797b9
tswicegood <tswicegood@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-01-05 18:44:58 +0000

    Adds support for primary diagnosis per final part of 4736.
    This doesn't seem to work as you'd expect.  Somehow the left join on coding_data is adding more results than without.  There's a double join in there somewhere, or possibly some bad data in one table or another.  The group by at the end fixes it by returning the same number of results as without the the join.  There appears to be bad data in coding_data.

--- local/setup/reports/OpenEncounterReport.sql
+++ local/setup/reports/OpenEncounterReport.sql
@@ -5,7 +5,8 @@ SELECT
 	e.encounter_id,
 	b.name AS "facility",
 	CONCAT_WS(', ', prov_person.last_name, prov_person.first_name) AS provider,
-	insurer.name AS insurance
+	insurer.name AS insurance,
+	c.code_text AS 'Primary Diagnosis'
 FROM
 	encounter AS e
 	INNER JOIN person AS p on e.patient_id = p.person_id
@@ -14,11 +15,16 @@ FROM
 	JOIN person AS prov_person USING(person_id)
 	LEFT JOIN storage_int AS curprog ON(e.encounter_id = curprog.foreign_key AND curprog.value_key = 'current_payer') 
 	LEFT JOIN insurance_program AS insurer ON(curprog.value = insurer.insurance_program_id)
+	LEFT JOIN coding_data AS cd ON (e.encounter_id = cd.foreign_id)
+	LEFT JOIN codes AS c ON (cd.code_id = c.code_id)
 WHERE
 	e.status = 'open' AND
+        (cd.primary_code = 1 OR cd.primary_code IS NULL) AND
 	IF ('[after]', e.date_of_treatment >= '[after:date]', 1) AND
 	IF ('[before]', e.date_of_treatment <= '[before:date]', 1) AND
 	IF ('[facility]', e.building_id = '[facility:query:SELECT id, name FROM buildings ORDER BY name]', 1) AND
 	IF ('[provider]', e.treating_person_id = '[provider:query:SELECT prov.person_id, CONCAT(per.first_name, " ", last_name) FROM provider AS prov JOIN person AS per USING(person_id)]', 1) AND
 	IF ('[insurance]', insurer.insurance_program_id = '[insurance:query:SELECT insurance_program_id, name FROM insurance_program WHERE LENGTH(name) > 0]', 1)
+GROUP BY
+	e.encounter_id
 
