4e80eba00385d240fd9dcdeb633abddd281aac06
cpowers <cpowers@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-01-10 17:16:12 +0000

    Removed old insert line

--- local/setup/sqltmp/clearhealth_report.sql
+++ local/setup/sqltmp/clearhealth_report.sql
@@ -7,7 +7,6 @@ CREATE TABLE `report_templates` (
   PRIMARY KEY  (`report_template_id`),
   KEY `report_id` (`report_id`)
 ) TYPE=MyISAM COMMENT='Report templates';
-;INSERT INTO `report_templates` VALUES (201803,17857,'Default Template','yes'),(17077,17075,'Invoice View','no'),(17859,17857,'MCC Superbill Form','no');
 CREATE TABLE `report_snapshot` (
   `report_snapshot_id` int(11) NOT NULL default '0',
   `report_id` int(11) NOT NULL default '0',
