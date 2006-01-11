0cd398a8b6caf7b0b37d763c5caeac5187c4e3c7
cpowers <cpowers@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-01-11 21:32:39 +0000

    Removed duplicate demo data

--- local/setup/sqltmp/demo_schedules.sql
+++ local/setup/sqltmp/demo_schedules.sql
@@ -5,7 +5,7 @@
 
 /*!40000 ALTER TABLE `schedules` DISABLE KEYS */;
 LOCK TABLES `schedules` WRITE;
-INSERT INTO `schedules` VALUES (502530,'NS','No Shows','This is primarily for reporting purposes. When a patients appointment is set to no-show. They are assigned to this schedule. You must have two events groups names \"No Shows\" and \"Cancelations\"','Schedule No Shows are assigned to',0,0,0),(502531,'ADM','Admin Events','Anything added to the admin schedule will appear on every calendar. Use this for practice-wide meetings.','Admin Events appear on every schedule',0,0,0),(502559,'PS','Dr Einstien\'s Schedule','Example Schedule do not use in production','',502539,502558,0),(502568,'PS','Dr Trotter\'s Schedule','','',0,502567,0),(502577,'PS','Dr Minsky\'s Schedule','Demo Schedule do not use in production. This one is particularly non-realistic. B/c this provider is in three places at once. Easier than having 10 providers though','',502539,502576,0);
+INSERT INTO `schedules` VALUES (502559,'PS','Dr Einstien\'s Schedule','Example Schedule do not use in production','',502539,502558,0),(502568,'PS','Dr Trotter\'s Schedule','','',0,502567,0),(502577,'PS','Dr Minsky\'s Schedule','Demo Schedule do not use in production. This one is particularly non-realistic. B/c this provider is in three places at once. Easier than having 10 providers though','',502539,502576,0);
 UNLOCK TABLES;
 /*!40000 ALTER TABLE `schedules` ENABLE KEYS */;
 
