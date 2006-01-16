fd8a247a4072f5d6b207eaf7044414a9c90b1166
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-01-16 19:35:04 +0000

    fee_scedule_discount tables

--- local/setup/sqltmp/clearhealth_fee.sql
+++ local/setup/sqltmp/clearhealth_fee.sql
@@ -26,3 +26,25 @@ CREATE TABLE `fee_schedule_revision` (
   PRIMARY KEY  (`revision_id`),
   KEY `user_id` (`user_id`)
 ) TYPE=MyISAM;
+CREATE TABLE `fee_schedule_discount` (
+  `fee_schedule_discount_id` int(11) NOT NULL default '0',
+  `practice_id` int(11) NOT NULL default '0',
+  `name` varchar(255) NOT NULL default '',
+  PRIMARY KEY  (`fee_schedule_discount_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `fee_schedule_discount_income` (
+  `fee_schedule_discount_income_id` int(11) NOT NULL default '0',
+  `fee_schedule_discount_id` int(11) NOT NULL default '0',
+  `fee_schedule_discount_level_id` int(11) NOT NULL default '0',
+  `family_size` int(11) NOT NULL default '0',
+  `income` float(9,2) NOT NULL default '0.00',
+  PRIMARY KEY  (`fee_schedule_discount_income_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `fee_schedule_discount_level` (
+  `fee_schedule_discount_level_id` int(11) NOT NULL default '0',
+  `fee_schedule_discount_id` int(11) NOT NULL default '0',
+  `discount` float(5,2) NOT NULL default '0.00',
+  `disp_order` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`fee_schedule_discount_level_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
