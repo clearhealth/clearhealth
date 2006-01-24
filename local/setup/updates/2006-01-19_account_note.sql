a0393304764ccb8ee2bb84c27696dca1ccefb7db
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-01-24 17:54:53 +0000

    fix sql

--- local/setup/updates/2006-01-19_account_note.sql
+++ local/setup/updates/2006-01-19_account_note.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE `account_note` (
   `account_note_id` int(11) NOT NULL default '0',
-  `patient_id` INT NOT NULL AFTER `account_note_id`,
+  `patient_id` INT(11) NOT NULL,
   `claim_id` varchar(100) NOT NULL default '',
   `user_id` int(11) NOT NULL default '0',
   `date_posted` datetime NOT NULL default '0000-00-00 00:00:00',
