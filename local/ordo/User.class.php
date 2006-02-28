c8f3effea977ce6813df4b1b7966d56c991ed3b5
ewestphall <ewestphall@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-02-28 21:12:52 +0000

    Modified structure only to comply with Uversa coding standards.

--- local/ordo/User.class.php
+++ local/ordo/User.class.php
@@ -27,69 +27,55 @@ class User extends Base_User {
 
    function users_factory($group="") {
                 
-                $users = array();
-                $u = new User(null,null);
-                $sql = "SELECT u.user_id from " . $u->_prefix . $u->_table . " as u ";                                                                                      
-                        
-                if (!empty($group)) {
-                  //op-en-hcs way of doing it
-                  //  $sql .= " LEFT JOIN ".$this->_prefix."users_groups as ug on ug.user_id=u.user_id LEFT JOIN ".$this->_prefix."groups as g on g.id = ug.group_id where g.name =" . $this->_db->qstr($group);
-                  if ($group =="provider") {
-                     //EW edited by eric 2-27-06
-                     //I hard coded the number two here but this is bad practice and 
-                     //should really be pulled dynamically
-                     //$sql = "SELECT u.user_id FROM provider INNER JOIN user u USING ( person_id ) ";
-                     
-                     $sql = "SELECT distinct(u.user_id )FROM  provider INNER JOIN user u 
-                              USING ( person_id ) 
-                              INNER JOIN person_type pt
-                              USING (person_id)
-                              WHERE pt.person_type = \"2\"";
+		$users = array();
+		$u = new User(null,null);
+		$sql = "SELECT u.user_id from " . $u->_prefix . $u->_table . " as u ";                                                                                      
 
+		if (!empty($group)) {
+			//op-en-hcs way of doing it
+			//  $sql .= " LEFT JOIN ".$this->_prefix."users_groups as ug on ug.user_id=u.user_id LEFT JOIN ".$this->_prefix."groups as g on g.id = ug.group_id where g.name =" . $this->_db->qstr($group);
+			if ($group =="provider") {
+				
+				$sql = "SELECT distinct(u.user_id )
+				FROM user AS u 
+					INNER JOIN person_type AS pt USING (person_id) 
+				WHERE pt.person_type = '2'";
 
-                  
-                  
-                  }
-                }
-                $sql .= " order by u.username";
-                 
-                 
-                
-                $results = $this->_db->Execute($sql) or die ("Database Error: " . $this->_db->ErrorMsg());
-                
-                
-                
-                
-                
-                while ($results && !$results->EOF) {
-                        $tu = new User("","");
-                        $tu->set("user_id",$results->fields['user_id']);
-                        $tu->populate();
-                        $users[] = $tu;
-                        $results->MoveNext();
-                }
-                return $users;
-        }
+			}
+		}
+		$sql .= " order by u.username";
 
-   /**
-    * Get the practiceId that matches this users default location, if no location is specified use the first practice
-    *
-    * @todo replace call to practices_factory with a setup method that returns the default practice
-    */
-   function get_DefaultPracticeId() {
-      $room =& Celini::newORDO('Room',$this->get('default_location_id'));
-      if ($room->isPopulated()) {
-         $building =& Celini::newORDO('Building',$room->get('building_id'));
-         return $building->get('practice_id');
-      }
-      else {
-         ORDataObject::factory_include('Practice');
-         $practices = Practice::practices_factory();
-         if (isset($practices[0])) {
-            return $practices[0]->get('id');
-         }
-      }
-      return 0;
-   }
+		$results = $this->_db->Execute($sql) or die ("Database Error: " . $this->_db->ErrorMsg());
+
+		while ($results && !$results->EOF) {
+		$tu = new User("","");
+		$tu->set("user_id",$results->fields['user_id']);
+		$tu->populate();
+		$users[] = $tu;
+		$results->MoveNext();
+		}
+		return $users;
+		}
+
+	/**
+	* Get the practiceId that matches this users default location, if no location is specified use the first practice
+	*
+	* @todo replace call to practices_factory with a setup method that returns the default practice
+	*/
+	function get_DefaultPracticeId() {
+		$room =& Celini::newORDO('Room',$this->get('default_location_id'));
+		if ($room->isPopulated()) {
+			$building =& Celini::newORDO('Building',$room->get('building_id'));
+			return $building->get('practice_id');
+		}
+		else {
+			ORDataObject::factory_include('Practice');
+			$practices = Practice::practices_factory();
+			if (isset($practices[0])) {
+				return $practices[0]->get('id');
+			}
+		}
+		return 0;
+	}
 }
 ?>
