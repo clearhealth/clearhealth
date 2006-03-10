31a7f480b9cb63214ccecb6fe0f36dfc7f4001fd
ewestphall <ewestphall@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-03-10 21:44:01 +0000

    This was not needed. I just turned it into a comment.

--- local/setup/updates/2006-03-10-insurance_program.sql
+++ local/setup/updates/2006-03-10-insurance_program.sql
@@ -1,4 +1,1 @@
-INSERT INTO `insurance_program` ( `insurance_program_id` , `payer_type` , `company_id` , `name` , `fee_schedule_id` , `x12_sender_id` , `x12_receiver_id` , `x12_version` , `address_id` , `funds_source` )
-VALUES (
-'0', '0', '0', 'None', '0', '', '', '', '0', '0'
-);
+#nevermind
