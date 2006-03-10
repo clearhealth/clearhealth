47042fddc7db7586adb70f71b0a8843feee30562
ewestphall <ewestphall@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-03-10 01:34:08 +0000

    Added type field to table to allow for default or program type.

--- local/setup/updates/2006-03-09-fee_schedule_discount.sql
+++ local/setup/updates/2006-03-09-fee_schedule_discount.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `fee_schedule_discount` ADD `insurance_program_id` INT( 11 ) NOT NULL AFTER `practice_id` ;
+ALTER TABLE `fee_schedule_discount` ADD `type` ENUM( 'default', 'program' ) NOT NULL AFTER `insurance_program_id` ;
