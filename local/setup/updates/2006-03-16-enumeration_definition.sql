4394a7d5c1a7566ec6f01b967f2c0dbb37f58cff
ewestphall <ewestphall@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-03-17 02:04:55 +0000

    updates enumeration definitions to support testing/production modes for billing

---
+++ local/setup/updates/2006-03-16-enumeration_definition.sql
@@ -0,0 +1,13 @@
+
+-- 
+-- Host: localhost
+-- Generation Time: Mar 16, 2006 at 05:53 PM
+-- Server version: 4.1.18
+-- PHP Version: 4.4.1-pl1
+-- 
+-- Database: `clearhealth`
+-- 
+
+
+
+INSERT INTO enumeration_definition VALUES (602407, 'billing_mode', 'Billing Mode', 'Mappedvalue');
