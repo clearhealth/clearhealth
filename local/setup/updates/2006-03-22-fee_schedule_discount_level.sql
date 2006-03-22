06a1793b6715d90d5d4684e188af2ee28d8b01f7
tswicegood <tswicegood@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-03-22 22:38:05 +0000

    Just because I like to commit multiple times.

--- local/setup/updates/2006-03-22-fee_schedule_discount_level.sql
+++ local/setup/updates/2006-03-22-fee_schedule_discount_level.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `fee_schedule_discount_level` ADD `type` ENUM( 'percent', 'enum' ) NOT NULL AFTER `discount` 
+ALTER TABLE `fee_schedule_discount_level` ADD `type` ENUM( 'percent', 'flat' ) NOT NULL AFTER `discount` 
