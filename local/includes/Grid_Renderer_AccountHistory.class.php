9ef2dc08a8855a04464a9cacc330959ed3360410
tswicegood <tswicegood@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-04-12 22:58:04 +0000

    Adds some changes to make this renderer work with the new MasterClaimList

--- local/includes/Grid_Renderer_AccountHistory.class.php
+++ local/includes/Grid_Renderer_AccountHistory.class.php
@@ -14,13 +14,15 @@ class Grid_Renderer_AccountHistory extends Grid_Renderer_HTML {
 		$ds =& $this->_grid->_datasource;
 		for($ds->rewind(); $ds->valid(); $ds->next()) {
 			$data = $ds->get();
-			$type = strtolower($data['type']);
+			$type = isset($data['type']) ? strtolower($data['type']) : '';
 			
 			if ($type == "claim") {
 				$this->_grid->attributes[$row]['tr']['class'] = "alt";				
 			}
 			else {
-				unset($this->_grid->attributes[$row]['tr']['class']);
+				if (isset($this->_grid->attrbiutes[$row]['tr']['class'])) {
+					unset($this->_grid->attributes[$row]['tr']['class']);
+				}
 			}
 			$ret .= $this->_renderTagWithAttributes('tr',$row,'tr');
 
@@ -29,13 +31,17 @@ class Grid_Renderer_AccountHistory extends Grid_Renderer_HTML {
 				$row++;
 			}
 			elseif ($this->indexCol) {
-				$this->_grid->attributes[$row]['td']['indexCol']['class'];
+				//$this->_grid->attributes[$row]['td']['indexCol']['class'];
 				$ret .= $this->_renderTagWithAttributes('td',$row,'indexCol')."</td>";
 			}
 
 			if (is_array($data)) {
 				foreach($ds->getRenderMap() as $index => $key) {
-					$ret .= $this->_renderTagWithAttributes('td',$row,$key)."$data[$key]</td>";
+					$ret .= $this->_renderTagWithAttributes('td',$row,$key);
+					if (isset($data[$key])) {
+						$ret .= $data[$key];
+					}
+					$ret .= '</td>';
 				}
 			}
 			$ret .= "</tr>\n";
