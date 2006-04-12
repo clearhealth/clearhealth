45eb41fbbbcb22ef44017eab1b933e47bd8d8fa8
gforsythe <gforsythe@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-04-12 22:16:33 +0000

    add zipcode js to ajax controller

--- local/post_bootstrap.php
+++ local/post_bootstrap.php
@@ -6,10 +6,11 @@ $ajax =& Celini::ajaxInstance();
 $ajax->jsLibraries[] = 'calendar';
 $ajax->jsLibraries[] = 'clniUtil';
 $ajax->jsLibraries[] = 'chBehaviors';
-
+$ajax->jsLibraries[] = 'zipcode';
 // Add application-specific js libraries
 $ajaxServer =& Celini::ajaxServerInstance();
 $ajaxServer->registerJSLibrary('chBehaviors', 'behavior.js', realpath(dirname(__FILE__) . '/..') . '/js/');
+$ajaxServer->registerJSLibrary('zipcode', 'zipcode.js', realpath(dirname(__FILE__) . '/..') . '/js/');
 
 // todo: where should this setting end up, does it really make sense to have it user configurable
 $conf =& Celini::configInstance();
