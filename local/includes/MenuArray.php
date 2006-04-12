f4050ee30c6d494b4b7ccaac4863b99273583b20
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-04-12 18:48:45 +0000

    pass along GET params and default value when changing practice #5296

--- local/includes/MenuArray.php
+++ local/includes/MenuArray.php
@@ -4,12 +4,29 @@ $menuarrays=array();
 if (isset($_SESSION['defaultpractice'])) {
 	$defPractice=&Celini::newOrdo('Practice',$_SESSION['defaultpractice']);
 	$trail =& Celini::trailInstance();
+
+	$get = '';
+	if (count($_GET) > 0) {
+		$get = $_SERVER['QUERY_STRING'];
+	}
+	if (isset($_GET['changepractice'])) {
+		$get = str_replace('changepractice='.$_GET['changepractice'],'',$get);
+	}
+	if ($get != '') {
+		if (substr($get,-1) != '&') {
+			$get .= '&';
+		}
+	}
+	$default = false;
+	if (isset($_GET[0])) {
+		$default = $_GET[0];
+	}
 	$menuarrays[]=array(
 		'menus'=>array(112,113,114,115),
 		'sql'=>"
 					SELECT 
 						p.name as title,
-						CONCAT('".Celini::link(true,true,true)."','changepractice=',p.id) as action,
+						CONCAT('".Celini::link(true,true,true,$default).$get."','changepractice=',p.id) as action,
 						p.id as item_id
 					FROM
 						practices AS p
