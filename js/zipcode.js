ba0180bf48dddcad25327aec63c2df83c0a45ac2
gforsythe <gforsythe@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-04-13 00:06:18 +0000

    changes to zipcode js for multiple addresses on one page

--- js/zipcode.js
+++ js/zipcode.js
@@ -1,18 +1,18 @@
-function zipGrab(zip) {
+function zipGrab(zip,addressid) {
 	HTML_AJAX.defaultEncoding = 'JSON'; // set encoding to JSON encoding method
-	HTML_AJAX.call('zipcode','getData',zipCallback,zip);
+	HTML_AJAX.call('zipcode','getData',zipCallback,zip,addressid);
 	HTML_AJAX.defaultEncoding = 'Null'; // return it to default which is Null
 }
 
 function zipCallback(resultSet) {
-	if(!resultSet==false){
-		document.getElementById('addresscity').value = resultSet['city'];
+	if(resultSet){
+		document.getElementById('zaddresscity'+resultSet['addressid']).value = resultSet['city'];
 		var i = 0;
-		while(i<document.getElementById('addressstate').options.length) {
-			if (document.getElementById('addressstate').options[i].innerHTML == resultSet['state']){
-				document.getElementById('addressstate').options[i].selected = true;
+		while(i<document.getElementById('zaddressstate'+resultSet['addressid']).options.length) {
+			if (document.getElementById('zaddressstate'+resultSet['addressid']).options[i].innerHTML == resultSet['state']){
+				document.getElementById('zaddressstate'+resultSet['addressid']).options[i].selected = true;
 			} else {
-				document.getElementById('addressstate').options[i].selected = false;
+				document.getElementById('zaddressstate'+resultSet['addressid']).options[i].selected = false;
 			}
 			i+=1;
 		}
