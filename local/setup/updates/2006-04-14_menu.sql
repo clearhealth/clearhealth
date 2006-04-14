2e7b893a5896b030ad3f586f3178ab2a5626c87a
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-04-14 21:56:26 +0000

    menu entries for the master account history report

---
+++ local/setup/updates/2006-04-14_menu.sql
@@ -0,0 +1,7 @@
+INSERT INTO `menu` ( `menu_id` , `site_section` , `parent` , `dynamic_key` , `section` , `display_order` , `title` , `action` , `prefix` )
+VALUES (
+'', 'billing', '42', '', 'children', '1', 'Master Account History', 'MasterAccountHistory/view', 'main'
+), (
+'', 'billing', '42', '', 'children', '0', 'Claim History', 'ClaimHistory/view', 'main'
+);
+
