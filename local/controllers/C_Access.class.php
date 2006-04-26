2adcf883c2845b3f817db8db830600504815090a
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-04-26 21:50:51 +0000

    C_Caledar doesn't exist anymore

--- local/controllers/C_Access.class.php
+++ local/controllers/C_Access.class.php
@@ -1,18 +1,12 @@
 <?php
 
 $loader->requireOnce('controllers/C_Base_Access.class.php');
-$loader->requireOnce('controllers/C_Calendar.class.php');
-$loader->requireOnce('controllers/M_Calendar.class.php');
 
 class C_Access extends C_Base_Access {
                                                                                 
 	// todo: figure out a generic way to do this
         function login_session_setup(&$user) {
 		if ($this->security->acl_qcheck('usage',false,'resources','calendar')) {
-			$c = new C_Calendar();
-			$m = new M_Calendar();
-			$m->setController($c);
-			$m->process_setFilter("","location/" . $user->get("default_location_id"));
 		}
 		else {
 			header('Location: '.Celini::link('find','PatientFinder'));
