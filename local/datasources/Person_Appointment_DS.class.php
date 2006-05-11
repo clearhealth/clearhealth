979db69479eebd9dad5d5648b2fd5947742c83d6
cpowers <cpowers@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-05-11 02:56:34 +0000

    Fixed DS to work with new cal

--- local/datasources/Person_Appointment_DS.class.php
+++ local/datasources/Person_Appointment_DS.class.php
@@ -33,8 +33,8 @@ class Person_Appointment_DS extends Datasource_sql {
 	var $_labels = array(
 			'start' 	=> 'Start',
 			'length'	=> 'Time',
-			'notes'		=> 'Title',
-			'reason_code'	=> 'Reason',
+			'title'		=> 'Title',
+			'reason'	=> 'Reason',
 			'provider'	=> 'Provider',
 			'location'	=> 'Location',
 		);
@@ -54,14 +54,14 @@ class Person_Appointment_DS extends Datasource_sql {
 							(mod(round(time_to_sec(end)-time_to_sec(start))/60,60)) ,'m') ),
 						concat(round((time_to_sec(end)-time_to_sec(start))/60),'m')
 					) length, concat(b.name,' -> ',r.name) location,
-					o.notes, reason_code, concat_ws(', ',p.last_name,p.first_name) provider ",
-			'from' => 'occurences o inner join rooms r on r.id = o.location_id inner join buildings b on b.id = r.building_id
-			left join user u on o.user_id = u.user_id left join person p on u.person_id = p.person_id',
+					a.title, reason, concat_ws(', ',p.last_name,p.first_name) provider ",
+			'from' => 'appointment a inner join event e on a.event_id = e.event_id inner join rooms r on r.id = a.room_id inner join buildings b on b.id = r.building_id
+			left join person p on a.provider_id = p.person_id ',
 			'orderby' => 'start DESC',
 			'where'   => ''
 			),
 		$this->_labels);
-		$this->registerFilter('reason_code',array(&$this,'reasonFilter'));
+		$this->registerFilter('reason',array(&$this,'reasonFilter'));
 
 		$enum =& ORDataObject::factory('Enumeration');
 		$this->reasonLookup = $enum->get_enum_list('appointment_reasons');
@@ -70,7 +70,7 @@ class Person_Appointment_DS extends Datasource_sql {
 
 	function prepare() {
 		settype($this->user_id,'int');
-		$this->_query['where'] .= " external_id = $this->user_id";
+		$this->_query['where'] .= " a.patient_id = $this->user_id";
 
 		if (!$this->showPast) {
 			$this->_query['where'] = " and start > date_format(now(),'%Y-%m-%d 00:00:00') ";
