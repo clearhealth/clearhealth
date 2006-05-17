665aab6126e8b3f775c4b86929952151e223ecbf
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-05-17 18:59:15 +0000

    menu's for patient merging

---
+++ local/setup/updates/2006-05-17_menu.sql
@@ -0,0 +1,4 @@
+INSERT INTO `menu` ( `menu_id` , `site_section` , `parent` , `dynamic_key` , `section` , `display_order` , `title` , `action` , `prefix` )
+VALUES (
+'', 'admin', '110', '', 'children', '100', 'Merge Duplicate Patients', 'PatientMerge/list', 'main'
+);
