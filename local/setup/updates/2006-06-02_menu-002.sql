66a5e6b4affd1b3c9ae12c520a00007261f41885
tswicegood <tswicegood@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-06-02 21:52:56 +0000

    Adds hidden entries for building/room controllers so they show up in admin section.

---
+++ local/setup/updates/2006-06-02_menu-002.sql
@@ -0,0 +1,3 @@
+INSERT INTO `menu` VALUES (61, 'admin', 43, '', 'children', 0, 'Building', 'Building/add', 'main');
+INSERT INTO `menu` VALUES (62, 'admin', 43, '', 'children', 0, 'Room', 'Room/add', 'main');
+
