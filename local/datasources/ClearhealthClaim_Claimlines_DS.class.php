4c2858dbaab6c6bba6df7e94a955365a8b4665a0
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-06-06 20:32:41 +0000

    show who coded the line and who billed it (billed is problematic since its only most recent)

--- local/datasources/ClearhealthClaim_Claimlines_DS.class.php
+++ local/datasources/ClearhealthClaim_Claimlines_DS.class.php
@@ -24,12 +24,20 @@ class ClearhealthClaim_Claimlines_DS extends Datasource_sql
 						fbcl.procedure,
 						fbcl.modifier,
 						fbcl.units,
-						fbcl.amount",
+						fbcl.amount,
+						u2.username coded,
+						u.username billed",
 				'from'    => "
 					clearhealth_claim cc
 					inner JOIN fbclaim  fbc on fbc.claim_identifier = cc.identifier
 					inner JOIN fblatest_revision fblr on fbc.claim_identifier = fblr.claim_identifier and fblr.revision = fbc.revision
 					inner JOIN fbclaimline fbcl on fbc.claim_id = fbcl.claim_id
+					LEFT JOIN ordo_registry AS oreg ON(fbcl.claimline_id = oreg.ordo_id)
+					LEFT JOIN user AS u ON(oreg.creator_id = u.user_id)
+					LEFT JOIN codes c ON(fbcl.procedure = c.code)
+					LEFT JOIN coding_data cd on (c.code_id = cd.code_id and cc.encounter_id = cd.foreign_id and parent_id = 0)
+					LEFT JOIN ordo_registry AS oreg2 ON(cd.coding_data_id = oreg2.ordo_id)
+					LEFT JOIN user AS u2 ON(oreg2.creator_id = u2.user_id)
 					",
 				'where'   => "cc.claim_id = {$claimId}"
 			),
@@ -37,7 +45,9 @@ class ClearhealthClaim_Claimlines_DS extends Datasource_sql
 				'procedure' => "Procedure",
 				'modifier' => "Modifier",
 				'units'	=> 'Units',
-				'amount' => 'Fee'
+				'amount' => 'Fee',
+				'coded' => 'Coded By',
+				'billed' => 'Billed By'
 			)	
 		);
 		
