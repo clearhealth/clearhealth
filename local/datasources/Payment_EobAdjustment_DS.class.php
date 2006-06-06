ff01ab3286254714dcf192f91dc6849d8eb75dfd
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-06-06 20:41:47 +0000

    add the user who entered the adjustment

--- local/datasources/Payment_EobAdjustment_DS.class.php
+++ local/datasources/Payment_EobAdjustment_DS.class.php
@@ -20,12 +20,17 @@ class Payment_EobAdjustment_DS extends Datasource_sql
 		
 		$this->setup(Celini::dbInstance(),
 			array(
-				'cols'    => "ifnull(codes.code,'Claim Level') code, eob_adjustment_type.value type, ea.value",
-				'from'    => "eob_adjustment ea ".$em->joinSql('eob_adjustment_type','adjustment_type').
-						" left join payment_claimline pc on ea.payment_claimline_id = pc.payment_claimline_id left join codes using(code_id)",
+				'cols'    => "ifnull(codes.code,'Claim Level') code, eob_adjustment_type.value type, ea.value, u.username user",
+				'from'    => "eob_adjustment ea ".
+						$em->joinSql('eob_adjustment_type','adjustment_type').
+						" left join payment_claimline pc on ea.payment_claimline_id = pc.payment_claimline_id 
+						left join codes using(code_id)
+						LEFT JOIN ordo_registry AS oreg ON(ea.eob_adjustment_id = oreg.ordo_id)
+						LEFT JOIN user AS u ON(oreg.creator_id = u.user_id)
+						",
 				'where'   => "ea.payment_id = {$paymentId}"
 			),
-			array('code' => 'Claimline', 'type' => 'Type','value'=>'Value'));
+			array('code' => 'Claimline', 'type' => 'Type','value'=>'Value', 'user'=>'Entered By'));
 	}
 }
 
