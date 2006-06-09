602aa362c2d7fcf30c64c39ee2b3abbb2fbcedad
tswicegood <tswicegood@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-06-09 17:05:27 +0000

    Adds logout to My Account menu

---
+++ local/setup/updates/2006-06-09-menu_001.sql
@@ -0,0 +1,4 @@
+INSERT INTO `menu` ( `menu_id` , `site_section` , `parent` , `dynamic_key` , `section` , `display_order` , `title` , `action` , `prefix` )
+VALUES (
+NULL , 'all', '55', '', 'children', '1000', 'Logout', 'Access/logout', 'main'
+);
