13abe5c6129231584e8edca8fe41e09346138897
tswicegood <tswicegood@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-06-12 18:18:42 +0000

    Adds Claim Template menu entry

---
+++ local/setup/updates/2006-06-12-menu_001.sql
@@ -0,0 +1,5 @@
+INSERT INTO `menu` ( `menu_id` , `site_section` , `parent` , `dynamic_key` , `section` , `display_order` , `title` , `action` , `prefix` )
+VALUES (
+NULL , 'admin', '37', '', 'children', '500', 'Claim Template', 'CodingTemplate/list', 'main'
+);
+
