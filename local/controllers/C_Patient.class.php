81f0cb32774033613d6402ffcf00adf0fbc43f77
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-06-14 16:29:39 +0000

    select patient

--- local/controllers/C_Patient.class.php
+++ local/controllers/C_Patient.class.php
@@ -352,6 +352,7 @@ class C_Patient extends Controller {
 			$lines[] = $res->fields;
 			$res->MoveNext();
 		}
+		$p =& Celini::newOrdo('Patient',$patientId);
 		
 		$ppplan =& Celini::newORDO('PatientPaymentPlan');
 		if($includeDependants == true) {
