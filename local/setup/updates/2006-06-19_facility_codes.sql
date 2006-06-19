70ece0a6b4759e5c6e25187d8ef0d34a4315fe2a
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-06-19 20:10:29 +0000

    facilty code data to load into chsuite

---
+++ local/setup/updates/2006-06-19_facility_codes.sql
@@ -0,0 +1,32 @@
+
+INSERT INTO `facility_codes` VALUES (1, '11', 'Office');
+INSERT INTO `facility_codes` VALUES (2, '12', 'Home');
+INSERT INTO `facility_codes` VALUES (3, '21', 'Inpatient Hospital');
+INSERT INTO `facility_codes` VALUES (4, '22', 'Outpatient Hospital');
+INSERT INTO `facility_codes` VALUES (5, '23', 'Emergency Room - Hospital');
+INSERT INTO `facility_codes` VALUES (6, '24', 'Ambulatory Surgical Center');
+INSERT INTO `facility_codes` VALUES (7, '25', 'Birthing Center');
+INSERT INTO `facility_codes` VALUES (8, '26', 'Military Treatment Facility');
+INSERT INTO `facility_codes` VALUES (9, '31', 'Skilled Nursing Facility');
+INSERT INTO `facility_codes` VALUES (10, '32', 'Nursing Facility');
+INSERT INTO `facility_codes` VALUES (11, '33', 'Custodial Care Facility');
+INSERT INTO `facility_codes` VALUES (12, '34', 'Hospice');
+INSERT INTO `facility_codes` VALUES (13, '41', 'Ambulance - Land');
+INSERT INTO `facility_codes` VALUES (14, '42', 'Ambulance - Air or Water');
+INSERT INTO `facility_codes` VALUES (15, '51', 'Inpatient Psychiatric Facility');
+INSERT INTO `facility_codes` VALUES (16, '52', 'Psychiatric Facility Partial Hospitalization');
+INSERT INTO `facility_codes` VALUES (17, '53', 'Community Mental Health Center');
+INSERT INTO `facility_codes` VALUES (18, '54', 'Intermediate Care Facility/Mentally Retarded');
+INSERT INTO `facility_codes` VALUES (19, '55', 'Residential Substance Abuse Treatment Facility');
+INSERT INTO `facility_codes` VALUES (20, '56', 'Psychiatric Residential Treatment Center');
+INSERT INTO `facility_codes` VALUES (21, '50', 'Federally Qualified Health Center');
+INSERT INTO `facility_codes` VALUES (22, '60', 'Mass Immunization Center');
+INSERT INTO `facility_codes` VALUES (23, '61', 'Comprehensive Inpatient Rehabilitation Facility');
+INSERT INTO `facility_codes` VALUES (24, '62', 'Comprehensive Outpatient Rehabilitation Facility');
+INSERT INTO `facility_codes` VALUES (25, '65', 'End Stage Renal Disease Treatment Facility');
+INSERT INTO `facility_codes` VALUES (26, '71', 'State or Local Public Health Clinic');
+INSERT INTO `facility_codes` VALUES (27, '72', 'Rural Health Clinic');
+INSERT INTO `facility_codes` VALUES (28, '81', 'Independent Laboratory');
+INSERT INTO `facility_codes` VALUES (29, '99', 'Other Unlisted Facility');
+        
+
