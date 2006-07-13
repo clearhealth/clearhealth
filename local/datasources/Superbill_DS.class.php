325b983504df0f68b0211b9b9dc898dfbbc5687f
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-07-13 19:22:57 +0000

    add the ability to optionally show the fee, fix order by

--- local/datasources/Superbill_DS.class.php
+++ local/datasources/Superbill_DS.class.php
@@ -5,7 +5,7 @@ class Superbill_DS extends Datasource_sql {
 	var $_internalName = 'Superbill_DS';
 	var $_type = 'html';
 
-	function Superbill_DS($id = false,$type = false) {
+	function Superbill_DS($id = false,$type = false,$feeScheduleId = false) {
 		$labels = array( 'code' => 'Code', 'code_text' => 'Code name');
 
 		if ($id === false) {
@@ -26,14 +26,26 @@ class Superbill_DS extends Datasource_sql {
 			$twhere .= " and c.code_type in(".implode(',',$tmp).") ";
 		}
 
+		$cols = 'c.code_id, c.code, code, code_text';
+		$from = 'codes c inner join superbill_data s using(code_id)';
+
+		if ($feeScheduleId) {
+			$f = enforceType::int($feeScheduleId);
+			$cols .= ', fsd.data fee';
+			$from .= " left join fee_schedule_data fsd on c.code_id = fsd.code_id and fsd.fee_schedule_id = $f";
+			$labels['fee'] = 'Fee';
+		}
+
 		$this->setup(Celini::dbInstance(),
 			array(
-				'cols' => 'c.code_id, c.code, code, code_text',
-				'from' => 'codes c inner join superbill_data s using(code_id)',
+				'cols' => $cols,
+				'from' => $from,
 				'where' => "s.status = 1 and s.superbill_id = $id $twhere",
-				'orderby' => 'code_type,code'
 			),
 			$labels);
+
+		$this->addDefaultOrderRule('code_type','ASC');
+		$this->addDefaultOrderRule('code','ASC',0);
 	}
 }
 ?>
