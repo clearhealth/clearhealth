bd9f4ac052d1e09f1338b65e3fc96f49e0aa4a8d
tswicegood <tswicegood@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-07-14 17:58:25 +0000

    Switch to snapshotId

--- local/setup/reports/PatientStatementHistory.sql
+++ local/setup/reports/PatientStatementHistory.sql
@@ -11,7 +11,7 @@ select
  statement_number `Statement #`,
  date_format(date_generated,'%m/%d/%Y') Date,
  amount,
-concat('<a href="report?report_id=',report_id,'&template_id=',template_id,'&fromSnapshot=',sh.report_snapshot_id,'">View</a>') View 
+concat('<a href="report?report_id=',report_id,'&template_id=',template_id,'&snapshotId=',sh.report_snapshot_id,'">View</a>') View 
 from
 statement_history sh
  left join report_snapshot using(report_snapshot_id)
