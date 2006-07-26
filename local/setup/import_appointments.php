dae9d1db621f52746b93e4e3d90fb50099466a00
tswicegood <tswicegood@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-07-26 23:01:56 +0000

    The base of the appointment importer, need to finish the insert code

---
+++ local/setup/import_appointments.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * An importer to handle upgrading all old (pre 1.0RC3) appointments to the new (1.0RC3) 
+ * appointments.
+*
+* @access private
+ */
+
+// comment out if you want to run via the web
+if (isset($_SERVER['HTTP_HOST'])) {
+	die('Unauthorized access prohibited');
+}
+
+// initial Celini environment
+define('APP_ROOT', realpath(dirname(__FILE__) . '/../../'));
+define('CELINI_ROOT', APP_ROOT . '/celini');
+define('CELLINI_ROOT', CELINI_ROOT);
+require_once CELINI_ROOT . '/bootstrap.php';
+
+
+$db = new clniDB();
+$oldAppointmentQuery = "
+	SELECT id,
+		event_id, start, end, notes, location_id, user_id, last_change_id, 
+		external_id, reason_code, timestamp, walkin, group_appointment
+	FROM
+		occurences
+	WHERE
+		external_id > 0";
+
+$oldAppointments = $db->execute($oldAppointmentQuery);
+$newAppointmentEntries = array();
+
+while ($oldAppointments && !$oldAppointments->EOF) {
+	echo "Found one\n";
+	$oldAppointment = $oldAppointments->fields;
+	
+	// store an array of all of the inserts
+	$oldAppointments->moveNext();
+}
+
+// genreate insert SQL
+
+?>
