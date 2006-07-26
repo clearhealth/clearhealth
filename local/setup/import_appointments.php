9d92076dd531440992ba98e5d9e09bf5eecf741d
tswicegood <tswicegood@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-07-26 23:30:13 +0000

    Adds a bit more to the script

--- local/setup/import_appointments.php
+++ local/setup/import_appointments.php
@@ -17,14 +17,14 @@ define('CELINI_ROOT', APP_ROOT . '/celini');
 define('CELLINI_ROOT', CELINI_ROOT);
 require_once CELINI_ROOT . '/bootstrap.php';
 
-$oldCHDB = 'clearhealth_old';
-$newCHDB = 'clearhealth';
+$GLOBALS['oldCHDB'] = 'clearhealth_old';
+$GLOBALS['newCHDB'] = 'clearhealth';
 
 
 $db = new clniDB();
 $oldAppointmentQuery = "
-	SELECT id,
-		event_id, start, end, notes, location_id, user_id, last_change_id, 
+	SELECT 
+		id, event_id, start, end, notes, location_id, user_id, last_change_id, 
 		external_id, reason_code, timestamp, walkin, group_appointment
 	FROM
 		{$oldCHDB}.occurences
@@ -38,10 +38,54 @@ while ($oldAppointments && !$oldAppointments->EOF) {
 	echo "Found one\n";
 	$oldAppointment = $oldAppointments->fields;
 	
-	// store an array of all of the inserts
+	$qAppointmentId = $db->quote($oldAppointment['id']);
+	$qTitle = $db->quote($oldAppointment['notes']);
+	$qReason = $db->quote($oldAppointment['reason_code']);
+	$qWalkin = $db->quote($oldAppointment['walkin']);
+	$qGroupAppointment = $db->quote($oldAppointment['group_appointment']);
+	$qCreatedDate = $db->quote($oldAppointment['timestamp']);
+	$qLastChangeId = $db->quote($oldAppointment['last_change_id']);
+	$qCreatorId = $qLastChangeId;
+	$qEventId = $db->quote($oldAppointment['event_id']);
+	$qProviderId = $db->quote($oldAppointment['user_id']);
+	$qPatientId = $db->quote($oldAppointment['external_id']);
+	$qRoomId = $db->quote($oldAppointment['location_id']);
+	$qPracticeId = $db->quote(getPracticeIdByRoomId($oldAppointment['location_id']));
+	
+	// todo: build insert from quoted values
+	
 	$oldAppointments->moveNext();
 }
 
-// genreate insert SQL
+// todo: genreate insert SQL
+
+
+/**
+ * Grabs the practice id based on a room id
+ *
+ * @param int
+ * @access private
+ */
+function getPracticeIdByRoomId($roomId) {
+	static $roomCache = array();
+	global $oldCHDB;
+	
+	if (!isset($roomCache[$roomId])) {
+		$db = new clniDB();
+		$qRoomId = $db->quote($roomId);
+		$sql = "
+			SELECT 
+				p.id
+			FROM
+				{$oldCHDB}.practices AS p
+				INNER JOIN {$oldCHDB}.buildings AS b ON(p.id = b.practice_id)
+				INNER JOIN {$oldCHDB}.rooms AS r ON(b.id = r.building_id)
+			WHERE
+				r.id = {$qRoomId}";
+		$roomCache[$roomId] = $db->getOne($sql);
+	}
+	
+	return $roomCache[$roomId];
+}
 
 ?>
