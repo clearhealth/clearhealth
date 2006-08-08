a48c6d27f0fa1d429133f46db5ab8b8b3a1d09d5
tswicegood <tswicegood@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-08-08 22:01:29 +0000

    Adds in final piece of the practice specific permissions - #5670

--- local/includes/acl/Auth.class.php
+++ local/includes/acl/Auth.class.php
@@ -3,10 +3,12 @@
  * @package com.uversainc.clearhealth
  */
  
-/**
- * Require abstract
+/**##@+
+ * Required file
  */
 $loader->requireOnce('includes/acl/AbstractAuth.abstract.php');
+$loader->requireOnce('includes/UserProfile.class.php');
+/**#@-*/
 
 /**
  * Provides Clearhealth specific authorization
@@ -27,7 +29,76 @@ class Auth extends AbstractAuth
 	 * {@inheritdoc}
 	 */
 	function can($who, $what, $where = null) {
-		return parent::can($who, $what, $where);
+		$override = Auth::_patientPermission($who, $what, $where);
+		return is_null($override) ? parent::can($who, $what, $where) : $override;
+	}
+	
+	
+	/**
+	 * Checks to see if the user has access to access a given patient's data.
+	 *
+	 * This will only attempt to insert itself into certain controllers.  If a action is being
+	 * performed on a controller where practice level permissions need to be applied, this will 
+	 * attempt to apply them.
+	 *
+	 * If possible, you want to implement this checking directly into to code as it will run faster
+	 * because the query can happen at the same time.  See {@link C_PatientFinder::SmartSearch()} 
+	 * for an example.  This serves as a back-up to directly accessing a Patient, Encounter, etc.
+	 *
+	 * @param  string  $who
+	 * @param  string  $what
+	 * @param  string  $where
+	 * @return boolean
+	 * @access private
+	 *
+	 * @todo Create an Auth::getInstance() so this information can be cached instead of having to
+	 *       query the DB each time a request comes through.
+	 */
+	function _patientPermission($who, $what, $where) {
+		if (empty($who)) {
+			return;
+		}
+		static $controllers = array('encounter', 'patient', 'patientdashboard');
+		if (!in_array(strtolower($where), $controllers)) {
+			return;
+		}
+		
+		$filteredGet =& Celini::filteredGet();
+		switch ($where) {
+			case 'encounter' :
+				$encounter =& Celini::newORDO('Encounter', $filteredGet->get('id'));
+				$patientId = $encounter->get('patient_id');
+				break;
+			
+			default:
+				$patientId = $filteredGet->get('id');
+				if (empty($patientId)) {
+					$patientId = $filteredGet->get(0);
+				}
+				break;
+		}
+		
+		$user =& Celini::newORDO('User', $who, 'ByUsername');
+		$userProfile =& new UserProfile($user->get('id'));
+		$db = new clniDB();
+		$qPatientId = $db->quote($patientId);
+		
+		$practiceList = implode(', ', $userProfile->getPracticeIdList());
+		$sql = "
+			SELECT
+				*
+			FROM
+				person AS psn
+				LEFT JOIN secondary_practice AS secondary ON(psn.person_id = secondary.person_id)
+			WHERE
+				psn.person_id = {$qPatientId} AND
+				(
+					psn.primary_practice_id IN({$practiceList}) OR 
+					secondary.practice_id IN({$practiceList})
+				)";
+		$result = $db->execute($sql);
+		
+		return !$result->EOF;
 	}
 }
 
