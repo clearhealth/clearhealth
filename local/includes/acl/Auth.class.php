37dc8b09ca489889f59917b1e733ecf3a3824484
tswicegood <tswicegood@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-08-08 17:23:31 +0000

    Adds stub for Clearhealth specific ACL system

---
+++ local/includes/acl/Auth.class.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package com.uversainc.clearhealth
+ */
+ 
+/**
+ * Require abstract
+ */
+$loader->requireOnce('includes/acl/AbstractAuth.abstract.php');
+
+/**
+ * Provides Clearhealth specific authorization
+ *
+ * @package com.uversainc.clearhealth
+ */
+class Auth extends AbstractAuth
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	function canI($what, $where = null) {
+		return parent::canI($what
+	}
+	
+	
+	/**
+	 * {@inheritdoc}
+	 */
+	function can($who, $what, $where = null) {
+		return parent::can($who, $what, $where);
+	}
+}
+
+?>
