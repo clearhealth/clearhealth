0679a3cdc9094242efd02526bfe8b8198f93b5bc
gforsythe <gforsythe@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-08-09 19:53:56 +0000

    secondarypractice gets processed in M_Patient, so don't need it in M_User

--- local/controllers/M_User.class.php
+++ local/controllers/M_User.class.php
@@ -22,13 +22,6 @@ class M_User extends M_Patient {
 		if (isset($_POST['providerToInsurance'])) {
 			$this->process_providerToInsurance_update($this->controller->person_id,$_POST['providerToInsurance']);
 		}
-		
-		if (isset($_POST['SecondaryPractice']) && $_POST['SecondaryPractice']['practice_id'] != '') {
-			$cSecondaryPractice =& new C_SecondaryPractice();
-			$cSecondaryPractice->process($_POST['SecondaryPractice']);
-			$action = empty($_POST['SecondaryPractice']['id']) ? 'Added' : 'Updated';
-			$this->messages->addMessage("Secondary Practice {$action}");
-		}
 	}
 	
 	
