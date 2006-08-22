0893421ba0bdf40f9707306f4b4b5f0697e585dd
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-08-22 20:13:01 +0000

    reporting menu entires so you don't switch sections when viewing a report

---
+++ local/setup/updates/2006-08-22_menu.sql
@@ -0,0 +1,4 @@
+INSERT INTO `menu` (`menu_id`, `site_section`, `parent`, `dynamic_key`, `section`, `display_order`, `title`, `action`, `prefix`) VALUES (75, 'billing', 1, '', 'children', 0, 'Reports', 'Billing/report', 'main'),
+(76, 'default', 1, '', 'children', 0, 'Reports', 'CalendarDisplay/report', 'main'),
+(77, 'patient', 1, '', 'children', 0, 'Reports', 'Patient/report', 'main');
+
