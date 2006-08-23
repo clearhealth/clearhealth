257c2fb1b8dc84851060fd0f49f9043561d693f1
jeichorn <jeichorn@61f7cdb9-23f1-0310-8583-e3fc15525073>
2006-08-23 20:08:00 +0000

    fix default sorting

--- local/datasources/Patient_EncounterList_DS.class.php
+++ local/datasources/Patient_EncounterList_DS.class.php
@@ -6,17 +6,6 @@ class Patient_EncounterList_DS extends Datasource_sql
 {
 	var $_encounterReasons = null;
 	
-	
-	/**
-	 * Stores the case-sensative class name for this ds and should be considered
-	 * read-only.
-	 *
-	 * This is being used so that the internal name matches the filesystem
-	 * name.  Once BC for PHP 4 is no longer required, this can be dropped in
-	 * favor of using get_class($ds) where ever this property is referenced.
-	 *
-	 * @var string
-	 */
 	var $_internalName = 'Patient_EncounterList_DS';
 	
 	/**
@@ -49,12 +38,13 @@ class Patient_EncounterList_DS extends Datasource_sql
 					patient_id = $patient_id AND
 					b.practice_id IN(" . implode(', ', $userPracticeList) . ")
 					",
-				'orderby' => 'date_of_treatment DESC'
 			),
 			array('date_of_treatment' => 'Date of Treatment','encounter_reason' => 'Reason', 'building' => 'Building', 'treating_person' => 'Treated By', 'status' => 'Status'/*,'encounter_id' => "Encounter Id"*/));
 
 		$this->orderHints['building'] = 'b.name';
+		$this->orderHints['date_of_treatment'] = 'e.date_of_treatment';
 		$this->registerFilter('encounter_reason',array(&$this,'encounterReason'));
+		$this->addDefaultOrderRule('date_of_treatment','DESC');
 	}
 	
 	
