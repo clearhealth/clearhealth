6c5c9eb722d00eba9045410bf9b157db4ddf9a44
tdrake <tdrake@61f7cdb9-23f1-0310-8583-e3fc15525073>
2007-02-15 01:32:11 +0000

    Added Criticals Pallet

--- local/ordo/WidgetForm.class.php
+++ local/ordo/WidgetForm.class.php
@@ -14,6 +14,7 @@ class WidgetForm extends ORDataObject {
 	var $name = '';
 	var $form_id		= '';
 	var $type		= '';
+	var $_widgetTypes = array();
 	/**#@-*/
 
 
@@ -30,14 +31,57 @@ class WidgetForm extends ORDataObject {
 	/**
 	 * Handle instantiation
 	 */
-	/*function WidgetForm() {
+	function WidgetForm() {
 		parent::ORDataObject();
-	}*/
+	}
 	
 	/*function setup() {
 		
 	}*/
 
-	
+        function getWidgetTypes() {
+                if (count($this->_widgetTypes) <= 0) {
+                        $enum = ORDataObject::factory('Enumeration');
+                        $this->_widgetTypes = $enum->get_enum_list('widget_type');
+                }
+                return $this->_widgetTypes;
+        }
+
+	function getFormIds() {
+		$db =& new clniDB();
+		$sql = "select form_id, concat(name, ' - ', description) as name from form";
+		$results = $db->execute($sql);
+                while ($results && !$results->EOF) {
+			$ret[$results->fields[form_id]] = $results->fields[name];
+			$results->MoveNext();
+		}
+		return $ret;
+	}
+
+	function getWidgetFields($form_id) {
+		$db =& new clniDB();
+		$sql = "select summary_column_id, type, name from summary_columns where form_id = '$form_id' order by name";
+		$results = $db->execute($sql);
+		while ($results && !$results->EOF) {
+			$ret[$results->fields[summary_column_id]] = $results->fields;
+			$results->MoveNext();
+		}
+		return $ret;
+
+	}
+
+	function getWidgetColumn($form_id) {
+		$db =& new clniDB();
+		$sql = "select max(summary_column_id) as summary_column_id from summary_columns where form_id = '$form_id'";
+		$results = $db->execute($sql);
+		$ret = $results->fields["summary_column_id"];
+
+		if (!$ret) {
+			$ret = 0;
+		}
+		return $ret;
+
+	}
+
 }
 ?>
