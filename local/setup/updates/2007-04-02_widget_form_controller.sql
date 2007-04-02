50bf903db76c09f2a49ea9305377bd6059fe87a0
duhlman <duhlman@61f7cdb9-23f1-0310-8583-e3fc15525073>
2007-04-02 19:02:36 +0000

    widget_form_controller table, code not fully comitted yet

---
+++ local/setup/updates/2007-04-02_widget_form_controller.sql
@@ -0,0 +1,47 @@
+-- MySQL dump 10.10
+--
+-- Host: localhost    Database: chtrunk
+-- ------------------------------------------------------
+-- Server version	5.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `widget_form_controller`
+--
+
+DROP TABLE IF EXISTS `widget_form_controller`;
+CREATE TABLE `widget_form_controller` (
+  `form_id` bigint(20) default NULL,
+  `controller_name` varchar(255) default NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `widget_form_controller`
+--
+
+
+/*!40000 ALTER TABLE `widget_form_controller` DISABLE KEYS */;
+LOCK TABLES `widget_form_controller` WRITE;
+INSERT INTO `widget_form_controller` VALUES (1080371,'PlannedCare'),(1080376,'RiskFactors'),(1080381,'Self_Mgmt_Goals');
+UNLOCK TABLES;
+/*!40000 ALTER TABLE `widget_form_controller` ENABLE KEYS */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
