7fede02606064cc78c6593761b2be7ae0b0a37fb
duhlman <duhlman@61f7cdb9-23f1-0310-8583-e3fc15525073>
2007-04-03 21:45:59 +0000

    convenience datasources primarily intended for soap/rest integration

---
+++ local/datasources/Person_AddressList_DS.class.php
@@ -0,0 +1,74 @@
+<?php
+$loader->requireOnce('includes/Datasource_sql.class.php');
+
+/**
+ * Displays a person's related person's addresses
+ *
+ * @package com.uversainc.clearhealth
+ */
+class Person_AddressList_DS extends Datasource_sql {
+	/**
+	 * {@inheritdoc}
+	 */
+	var $_internalName = 'Person_AddressList_DS';
+	
+	/**
+	 * The default output type for this datasource.
+	 *
+	 * @var string
+	 */
+	var $_type = 'html';
+	
+	var $_personId = '';
+	
+	function Person_AddressList_DS($person_id) {
+		$this->_personId = $person_id;
+		
+		$qPersonId = clniDB::quote($person_id);
+		$this->setup(Celini::dbInstance(),
+			array(	'cols' 	=> "
+							t.person_id,
+							a.address_id,
+							pa.address_type,
+							a.line1,
+							a.line2,
+							a.city,
+							a.postal_code",
+						'from' 	=> "
+							person AS t
+							INNER JOIN person_address AS pa ON (pa.person_id=t.person_id)
+							INNER JOIN address AS a USING(address_id)", 
+						'where'	=> "t.person_id = {$qPersonId}"
+			,
+			array(
+				'address_type' => 'Type',
+				'line1' => 'Address',
+				'line2' => 'Address2',
+				'city' => 'City',
+				'postal_code' => 'Zip'
+			))
+		);
+		
+		//var_dump($this->preview());
+		$this->registerFilter('address_type', array(&$this, '_addressTypeLookup'));
+		$this->registerFilter('line1', array(&$this, '_twoLineFormatting'));
+	}
+	
+	
+	function _twoLineFormatting($value, $row) {
+		if (!isset($row['line2']) || empty($row['line2'])) {
+			return $value;
+		}
+		
+		return $value . ', ' . $row['line2'];
+	}
+	
+	
+	function _addressTypeLookup($value) {
+		$em =& Celini::enumManagerInstance();
+		return $em->lookup('address_type', $value);
+	}
+	
+	
+}
+?>
---
+++ local/datasources/Person_ParticipationProgram_DS.class.php
@@ -0,0 +1,47 @@
+<?php
+$loader->requireOnce('/includes/Datasource_sql.class.php');
+
+/**
+ * @package com.uversainc.celini
+ */
+class Person_ParticipationProgram_DS extends Datasource_Sql {
+	
+	var $primaryKey = 'person_participation_program_id';
+	var $_internalName = 'Person_ParticipationProgram_List_DS';
+	var $_widgetTypes = 0;
+
+	/**
+	 * The default output type for this datasource.
+	 *
+	 * @var string
+	 */
+	var $_type = 'html';
+
+
+	function Person_ParticipationProgram_DS($person_id) {
+		$this->_personId = $person_id;
+
+                $qPersonId = clniDB::quote($person_id);
+		$this->setup(Celini::dbInstance(),
+			array(
+				'cols'    => "*",
+				'from'    => "person_participation_program p2pp
+					     INNER JOIN participation_program pprog on pprog.participation_program_id = p2pp.participation_program_id",
+				'orderby' => 'pprog.name',
+				'where'   => ' p2pp.person_id = ' . $qPersonId
+			),
+			array('name' => 'Name', 'start' => 'Start', 'end' => 'End','expires'=> 'Expires','active'=>'Active'));
+			$this->registerTemplate('name','<a href="'.substr(Celini::link('editConnect','ParticipationProgram',true,false),0,-1).'/{$person_program_id}">{$name}</a>');
+			$this->registerFilter('expires',array(&$this,'_yesno'));
+			$this->registerFilter('active',array(&$this,'_yesno'));
+
+	}
+
+        function _yesno($value) {
+                $ret = "no";
+		if ($value == 1) $ret = "yes";
+		return $ret;
+        }
+}
+
+?>
