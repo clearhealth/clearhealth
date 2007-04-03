f73b1bb262d69eb43a24f4fb0c19feca88f3f6d9
duhlman <duhlman@61f7cdb9-23f1-0310-8583-e3fc15525073>
2007-04-03 21:49:23 +0000

    new participation program features

---
+++ local/includes/ParticipationProgramAbstract.class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Abstract Specialty Participation Program implementation
+ *
+ * 
+ *
+ * @package     com.clearhealth.base
+ *
+ */
+
+class ParticipationProgramAbstract extends ORDataObject {
+
+	/**
+         * Stores the type, if known, of output being generated
+         *
+         * @var string|null
+	*/
+
+	function actionOptionsPanel_edit() {
+
+	}
+	
+	function processOptionsPanel_edit() {
+
+	}
+
+	function _createTables() {
+
+	}
+}
+?>
