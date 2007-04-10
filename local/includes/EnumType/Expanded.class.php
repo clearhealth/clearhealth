e3070cc86f4a0e41587558f4b828a1066b04966d
duhlman <duhlman@61f7cdb9-23f1-0310-8583-e3fc15525073>
2007-04-10 05:57:14 +0000

    new enum option type

---
+++ local/includes/EnumType/Expanded.class.php
@@ -0,0 +1,28 @@
+<?php
+$loader->requireOnce('includes/EnumType/PerPractice.class.php');
+/**
+ * Class the defines the default enumeration type
+ */
+class EnumType_Expanded extends EnumType_PerPractice {
+
+	/**
+	 * Field info map, array of field names and types to use when editing
+	 */
+	var $definition = array(
+                'enumeration_value_id' => array('type'=>'hidden'),
+                'key'     => array('type' => 'hidden'), 
+                'value' => array('label'=>'FPL','size'=>5),
+                'extra1' => array(
+                    'label' => 'Visit Fee&nbsp;',
+                    'size'  => 5
+                ),
+                'extra2' => array(
+                    'label' => 'Proc. Fee&nbsp;',
+                    'size'  =>5
+                ),
+                'sort' => array('label'=>'Order&nbsp;','type'=>'order'),
+                'status' => array('label'=>'Enabled','type'=>'boolean')
+            );
+
+}
+?>
