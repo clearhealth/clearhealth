1c2e726f6e146843e7bdf343370d19c7734a32c3
duhlman <duhlman@61f7cdb9-23f1-0310-8583-e3fc15525073>
2007-05-01 17:56:00 +0000

    link formdata to encounters etc.

---
+++ local/datasources/FormDataByExternalByFormId_DS.class.php
@@ -0,0 +1,44 @@
+<?php
+$loader->requireOnce('includes/Datasource_sql.class.php');
+
+/**
+ * Displays a person's related person's addresses
+ *
+ * @package com.uversainc.clearhealth
+ */
+class FormDataByExternalByFormId_DS extends Datasource_sql {
+	/**
+	 * {@inheritdoc}
+	 */
+	var $_internalName = 'FormDataByExternalByFormId_DS';
+	
+	/**
+	 * The default output type for this datasource.
+	 *
+	 * @var string
+	 */
+	var $_type = 'html';
+	
+	
+	function FormDataByExternalByFormId_DS($externalId, $formId) {
+		$externalId = (int)$externalId;
+		$formId = (int)$formId;
+		
+		$this->setup(Celini::dbInstance(),
+			array(
+                                'cols'    => "last_edit, f.name, form_data_id, external_id",
+                                'from'    => " form_data AS d
+                                             INNER JOIN form AS f USING(form_id)
+                                             ",
+                                'orderby' => 'last_edit DESC',
+                                'where'   => "external_id = {$externalId} and form_id = {$formId}"
+                        ),
+                        array('name' => 'Form Name','last_edit'=>'Last Edit'
+			));
+		
+		//var_dump($this->preview());
+	}
+	
+	
+}
+?>
