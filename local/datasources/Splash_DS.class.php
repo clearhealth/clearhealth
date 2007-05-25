8ca904b4bc97fb12e059b89d8285bd4cd5406fd7
duhlman <duhlman@61f7cdb9-23f1-0310-8583-e3fc15525073>
2007-05-25 21:35:37 +0000

    splash

---
+++ local/datasources/Splash_DS.class.php
@@ -0,0 +1,42 @@
+<?php
+$loader->requireOnce('includes/Datasource_sql.class.php');
+
+/**
+ * Displays a person's related person's addresses
+ *
+ * @package com.uversainc.clearhealth
+ */
+class Splash_DS extends Datasource_sql {
+	/**
+	 * {@inheritdoc}
+	 */
+	var $_internalName = 'Splash_DS';
+	
+	/**
+	 * The default output type for this datasource.
+	 *
+	 * @var string
+	 */
+	var $_type = 'html';
+	
+	
+	function Splash_DS() {
+		
+		$this->setup(Celini::dbInstance(),
+			array(	'cols' 	=> "
+							*
+							",
+						'from' 	=> "
+							splash sp",
+						'where'	=> ""
+			),
+			array(
+				'name' => 'Name',
+				'splash_id' => 'ID'
+			)
+		);
+		$this->registerTemplate('splash_id','<a href="' .Celini::link('edit') . 'splash_id={$splash_id}">{$splash_id}</a>');
+		
+	}
+}
+?>
