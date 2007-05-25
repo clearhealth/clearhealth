ec7deced714b8b24b70c2bb04eea6f544b75efaf
duhlman <duhlman@61f7cdb9-23f1-0310-8583-e3fc15525073>
2007-05-25 21:35:28 +0000

    splash screen

---
+++ local/ordo/Splash.class.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Object Relational Persistence Mapping Class for table: splash
+ *
+ * @package	com.uversainc.celini
+ * @author	Uversa Inc.
+ */
+class Splash extends ORDataObject {
+
+	/**#@+
+	 * Fields of table: splash mapped to class members
+	 */
+	var $splash_id		= '';
+	var $name		= '';
+	var $message		= '';
+	/**#@-*/
+
+
+	/**
+	 * DB Table
+	 */
+	var $_table = 'splash';
+
+	/**
+	 * Primary Key
+	 */
+	var $_key = 'splash_id';
+	
+	/**
+	 * Internal Name
+	 */
+	var $_internalName = 'Splash';
+
+	/**
+	 * Handle instantiation
+	 */
+	function Splash() {
+		parent::ORDataObject();
+	}
+	function setup() {
+		$sql = "select splash_id from splash limit 1";
+		$db = new clniDB();
+		$res = $db->execute($sql);
+		if ($res && !$res->EOF) {
+			parent::setup($res->fields['splash_id']);
+		}
+		else {
+			parent::setup(0);
+		}
+	}
+
+	
+}
+?>
