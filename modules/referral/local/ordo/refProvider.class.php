e23f3d4962437e568ad207ae5bd3adcc5df14c72
duhlman <duhlman@61f7cdb9-23f1-0310-8583-e3fc15525073>
2007-08-10 08:05:35 +0000

    provider cleanup

--- modules/referral/local/ordo/refProvider.class.php
+++ modules/referral/local/ordo/refProvider.class.php
@@ -148,13 +148,13 @@ class refProvider extends ORDataObject
 			if (!in_array($enumValue->key, $this->_specialtyKeys)) {
 				continue;
 			}
-			$valueSQL[] = '"refprovider", "' . $this->get('id') . '", "' . $enumValue->enumeration_value_id . '"';
+			$valueSQL[] = '(' .$this->_db->nextId() . ',"refprovider", "' . $this->get('id') . '", "' . $enumValue->enumeration_value_id . '")';
 		}
 		
 		$sql = '
 			INSERT INTO refSpecialtyMap 
 				(refSpecialityMap_id, external_type, external_id, enumeration_value_id) 
-			VALUES (' . $this->_db->nextId() . ',' . implode(', ', $valueSQL) . ")";
+			VALUES ' . implode(', ', $valueSQL);
 		$this->_db->Execute($sql);
 		// handle mapping of specialist enum to refSpecialtyMap
 	}
