4a037c41695fb72cc242b8d1d80f4e3878ae5453
devteam <devteam@61f7cdb9-23f1-0310-8583-e3fc15525073>
2007-12-05 19:02:09 +0000

    field lengths

--- local/setup/ch_2_1_install.sql
+++ local/setup/ch_2_1_install.sql
@@ -613,7 +613,7 @@ CREATE TABLE `coding_data` (
   `foreign_id` int(11) NOT NULL default '0',
   `parent_id` int(11) NOT NULL default '0',
   `code_id` int(11) NOT NULL default '0',
-  `modifier` int(11) NOT NULL default '0',
+  `modifier` varchar(255) NOT NULL default '0',
   `units` float(5,2) NOT NULL default '1.00',
   `fee` float(11,2) NOT NULL default '0.00',
   `primary_code` tinyint(4) NOT NULL default '0',
