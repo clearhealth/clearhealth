b1f3a8d12edd7b7786395c08ee96984a74573442
duhlman <duhlman@61f7cdb9-23f1-0310-8583-e3fc15525073>
2008-02-13 22:23:27 +0000

    rss page type driver

---
+++ celini/controllers/C_XML.class.php
@@ -0,0 +1,29 @@
+<?php
+
+$loader->requireOnce('controllers/C_Main.class.php');
+
+class C_XML extends C_Main
+{
+	function C_XML() {
+		$this->C_Main('xml');
+		$this->view->path = 'main';
+	}
+	
+	
+	/**
+	 * Serves as the display portion of this controller.
+	 *
+	 * If $_GET['embedded'] is specified, this will strip out all html, head,
+	 * and body tags.
+	 *
+	 * @param  string
+	 * @return string
+	 */
+	function display($display = '') {
+		$return = parent::display($display);
+		if ($this->GET->exists('embedded')) {
+			$return = preg_replace('/<([\/]?)(html|head|body)([^>]*)>/', '', $return);
+		}
+		return $return;
+	}
+}
