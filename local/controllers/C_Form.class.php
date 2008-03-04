29390ed00e77f426f9109043e6a0029b5e4fc128
devteam <devteam@61f7cdb9-23f1-0310-8583-e3fc15525073>
2008-03-04 22:12:02 +0000

    form pdf fixes

--- local/controllers/C_Form.class.php
+++ local/controllers/C_Form.class.php
@@ -1,6 +1,7 @@
 <?php
 
 $loader->requireOnce('includes/Grid.class.php');
+$loader->requireOnce('includes/ReportAction.class.php');
 
 /**
  * Controller for generic form processing
@@ -76,19 +77,23 @@ class C_Form extends Controller {
 		$this->messages->addMessage('Form Added Successfully');
 
 		if (isset($_FILES['form']['tmp_name']['upload_form']) && strlen($_FILES['form']['tmp_name']['upload_form']) > 0) {
-			$filename = $form->get('file_path');
+			$file_command = Celini::config_get("document_manager:file_command_path");
+                        $cmd_args = "-i ".escapeshellarg($_FILES['form']['tmp_name']['upload_form']);
+                        $command = $file_command." ".$cmd_args;
+                        $mimetype = exec($command);
+                        $mime_array = split(":", $mimetype);
+                        $ext = "html";
+                        if (is_array($mime_array) && count($mime_array) > 1) {
+                        	$mimetype = $mime_array[1];
+                                if (stripos($mimetype,"pdf") !== false) $ext = "pdf";
+                        }
 
-			if (!move_uploaded_file($_FILES['form']['tmp_name']['upload_form'],$filename)) {
+			if (!move_uploaded_file($_FILES['form']['tmp_name']['upload_form'], $form->get_file_path($ext))) {
 				$this->messages->addMessage('Problem Uploading Form');
 			}
 			else {
 						$form_structure_id = 0;
 						
-//						$structure=& Celini::newOrdo('FormStructure',$form_structure_id);
-//						$structure->set('form_id', $this->form_id);
-//						$structure->set ('form_structure_id', 0);
-//						$structure->getFieldsList ($filename);
-			
 				$this->messages->addMessage('Form Uploaded Successfully');
 			}
 		}
@@ -130,6 +135,61 @@ class C_Form extends Controller {
 		$form_data_id = EnforceType::int($form_data_id);
 		$externalId = $this->GET->get("externalId","int");
 		$encounterId = $this->GET->get("encounterId","int");
+		$data = '';
+		$retTo = "";
+		if ($this->GET->exists('returnTo')) {
+			$this->assign('returnTo',$this->GET->get('returnTo'));
+			$retTo = "&returnTo=".$this->GET->get('returnTo');
+		}
+
+		$form =& ORDataObject::factory('Form',$form_id);
+		$this->assign('formId',$form->get('form_id'));
+		if ($form_id == false) {
+			$ds =& $form->formList();
+			$ds->template['name'] = '<a href="'.Celini::link('fillout').'id={$form_id}">{$name}</a>';
+			$grid =& new cGrid($ds);
+			$this->assign_by_ref('grid',$grid);
+		}
+		else {
+			if (isset($this->form_data_id)) {
+				$form_data_id = $this->form_data_id;
+			}
+			$data =& ORDataObject::factory('FormData',$form_data_id);
+			$data->set('form_id',$form_id);
+			$formAction = Celini::link('fillout',true,true,$form_id)."form_data_id=$form_data_id$retTo";
+			if ($externalId > 0) {
+				$formAction .= "&externalId=$externalId";
+				$data->set('external_id',$externalId); 
+			}
+			elseif ($this->get('external_id','c_patient')>0) {
+				$this->externalId = $this->get('external_id','c_patient');
+				$data->set('external_id',$this->externalId); 
+			}
+			if ($encounterId > 0) {
+				$formAction .= "&encounterId=$encounterId"; 
+				$data->set('encounter_id',$encounterId); 
+			}
+			$this->assign('FORM_ACTION',$formAction);
+			
+			$this->assign_by_ref('form',$form);
+			$this->assign_by_ref('data',$data);
+		}
+		$filename = $form->get('file_path');
+                if (substr($filename,-3) === "pdf") {
+		$data->strings = $data->_string_storage->_values;
+		$data->ints = $data->_int_storage->_values;
+		$data->dates = $data->_date_storage->_values;
+		$data->texts = $data->_text_storage->_values;
+                header("Content-type: application/vnd.adobe.xfdf");
+                $this->_makeFormXml($data,$form->get('system_name'),$form->get('file_path'));
+                }
+		return $this->view->render("fillout.html");
+	}
+	function actionFilloutCombined_edit($form_id = 0,$form_data_id = 0, $reportId=0) {
+		$form_data_id = EnforceType::int($form_data_id);
+		$externalId = $this->GET->get("externalId","int");
+		$encounterId = $this->GET->get("encounterId","int");
+		$reportId = $this->GET->get('reportId',"int");
 
 		$retTo = "";
 		if ($this->GET->exists('returnTo')) {
@@ -161,16 +221,88 @@ class C_Form extends Controller {
 			
 			$this->assign_by_ref('form',$form);
 			$this->assign_by_ref('data',$data);
-			$this->secure_dir[] = APP_ROOT."/user/form/";
+		}
+		if ($reportId > 0) {
+			$ra = new ReportAction();
+                	$ra->controller = new Controller();
+                	$ra->fetch = false;
+                	//0 is to use default template id
+                	$ra->action($reportId, 0);
+			foreach ($ra->reports as $report_name => $report) {
+				$data->$report_name = $report['ds']->toArray();
+			}
 		}
 
-
+		$filename = $form->get('file_path');
+                if (substr($filename,-3) === "pdf") {
+                header("Content-type: application/vnd.adobe.xfdf");
+                $this->_makeFormXml($data,$form->get('system_name'),$form->get('file_path'));
+                }
 		return $this->view->render("fillout.html");
+}
+
+	function actionPreview_view($formId = 0) {
+		$formId = (int)$formId;
+
+		$form =& ORDataObject::factory('Form',$formId);
+		$this->assign('formId',$form->get('form_id'));
+		if ($formId == false) {
+			$ds =& $form->formList();
+			$ds->template['name'] = '<a href="'.Celini::link('fillout').'id={$formId}">{$name}</a>';
+			$grid =& new cGrid($ds);
+			$this->assign_by_ref('grid',$grid);
+		}
+		else {
+			$this->assign_by_ref('form',$form);
+		}
+		$filename = $form->get('file_path');
+		if (substr($filename,-3) === "pdf") {
+                header("Content-type: application/vnd.adobe.xfdf");
+		$this->_makeFormXml(array(),$form->get('system_name'),$form->get('file_path'));
+		}
+		return $this->view->render("fillout.html");
+	}
+
+	function _makeFormXml($data=array(),$name="",$filename="") {
+		$header =
+'<?xml version="1.0" encoding="UTF-8"?><?xfa generator="XFA2_4" APIVersion="2.6.7116.0"?>
+<xdp:xdp xmlns:xdp="http://ns.adobe.com/xdp/" timeStamp="2008-01-16T02:06:28Z" uuid="6aee0086-4ab9-40a0-8119-5a0f3d39220a">
+<xfa:datasets xmlns:xfa="http://www.xfa.org/schema/xfa-data/1.0/">
+<xfa:data>
+<form1>';
+$str = '';
+                $str = ORDataObject::toXML($data,$name);
+$str .=
+'</form1>
+</xfa:data>
+</xfa:datasets>
+<pdf href="'. $this->view->_tpl_vars['base_uri'] ."index.php/Images/".basename($filename).'" xmlns="http://ns.adobe.com/xdp/pdf/" />
+</xdp:xdp>';
+                echo $header.$str;exit;
 	}
 
 	function processFillout_edit($form_id = 0, $form_data_id = 0) {
 			$data =& ORDataObject::factory('FormData',$form_data_id);
-			$data->populate_array($_POST);
+			if (isset($_POST) && strtolower($_POST['form_type']) === 'pdf') {
+                        $int = array();
+                        $string = array();
+                        $date = array();
+                        $text = array();
+                        foreach ($_POST as $key => $var) {
+                                if (preg_match('/^(string|int|date|text)_(.*)/',$key,$match)) {
+                                        if ($match[1] === "int") $int[$key] = $var;
+                                        if ($match[1] === "string") $string[$key] = $var;
+                                        if ($match[1] === "date") $date[$key] = $var;
+                                        if ($match[1] === "text") $text[$key] = $var;
+                                }
+                        }
+                        $pdfData = array("string" => $string, "int" =>$int, "date" => $date, "text" => $text);
+                        $data->populate_array($pdfData);
+                        }
+                        else {
+                        $data->populate_array($_POST);
+                        }
+
 			$data->set('form_id',$form_id);
 			
 			if (!$data->get('external_id') >0 && $this->_external_id >0) {
@@ -182,29 +314,13 @@ class C_Form extends Controller {
 			$data->set('last_edit',date('Y-m-d H:i:s'));
 			if ($this->GET->get("encounterId","int") > 0) {
 				$data->set("encounter_id",$this->GET->get("encounterId","int"));
-				
 			}
 			elseif ($this->_encounter_id > 0) {
 				$data->set("encounter_id",$this->_encounter_id);
-			
 			}
 			$data->persist();
 			
-//			$structure=& Celini::newOrdo('FormStructure');
-//			$structure->set('form_id', $form_id);
-//			$form_structure = $structure->build_form_structure_array ($form_data_id);
-			//print_r ($form_structure);
-			
-			$formRule = & ORDataObject::factory('FormRule');
-			
-			/*foreach ($form_structure as $field_name => $field_value) {
-				$messages = $formRule->checkFieldRule ($field_name, $field_value);
-				if (is_array($messages)){
-					foreach ($messages as $message) {
-						$this->messages->addMessage($message);
-					}
-				}
-			}*/
+			//$formRule = & ORDataObject::factory('FormRule');
 			
 			$this->messages->addMessage('Form Updated');
 			$this->form_data_id = $data->get('id');
