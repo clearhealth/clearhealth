135cab510613080ec0d658aa0b7222c464fd889a
devteam <devteam@61f7cdb9-23f1-0310-8583-e3fc15525073>
2008-03-06 00:09:28 +0000

    passthrough to get session values through report controller call tokens

---
+++ celini/controllers/C_Session.class.php
@@ -0,0 +1,29 @@
+<?php
+class C_Session extends Controller {
+
+	function get($value,$default=false,$parent=NULL) {
+		if (is_null($parent)) $parent = $_SESSION;
+                $vars = split(':',$value);
+                if (count($vars) > 0 && !empty($vars[1])) {
+				if (is_object($parent)) { $parent=$parent->$vars[0];}
+				else {
+                                $parent = $parent[$vars[0]];
+				}
+                                array_shift($vars);
+                                return C_Session::get(implode(":",$vars),$default,$parent);
+                }
+                elseif (is_array($parent) && isset($parent[$value]))  {
+                        return $parent[$value];
+                }
+		elseif (is_object($parent) && isset($parent->$value)) {
+                        return $parent->$value;
+		}
+                return $default;
+	}
+	
+	/*function actionTest($arg="") {
+		var_dump( $this->get("frame:me:_objects:user:default_location_id"));
+	}*/
+
+}
+?>
