b5c780858728a6ffa6cd022c4168f5cf1e4eafd9
devteam <devteam@61f7cdb9-23f1-0310-8583-e3fc15525073>
2008-05-27 20:50:57 +0000

    c_session for use as report token

--- celini/controllers/C_Session.class.php
+++ celini/controllers/C_Session.class.php
@@ -3,14 +3,14 @@ class C_Session extends Controller {
 
 	function get($value,$default=false,$parent=NULL) {
 		if (is_null($parent)) $parent = $_SESSION;
-                $vars = split(':',$value);
+                $vars = split('//',$value);
                 if (count($vars) > 0 && !empty($vars[1])) {
 				if (is_object($parent)) { $parent=$parent->$vars[0];}
 				else {
                                 $parent = $parent[$vars[0]];
 				}
                                 array_shift($vars);
-                                return C_Session::get(implode(":",$vars),$default,$parent);
+                                return C_Session::get(implode("//",$vars),$default,$parent);
                 }
                 elseif (is_array($parent) && isset($parent[$value]))  {
                         return $parent[$value];
@@ -22,7 +22,7 @@ class C_Session extends Controller {
 	}
 	
 	/*function actionTest($arg="") {
-		var_dump( $this->get("frame:me:_objects:user:default_location_id"));
+		var_dump( $this->get("frame//me//_objects//user//id"));
 	}*/
 
 }
